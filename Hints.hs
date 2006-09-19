6bc4d83fa8ece3da2a3cf5bc33a3f877b6764077
Neil Mitchell <ndmitchell@gmail.com>
2006-09-19 11:26:10 +0000

    Add more general matching, allow integers as well

--- Hints.hs
+++ Hints.hs
@@ -7,3 +7,6 @@ map_map f g x = map f (map g x)
 
 box_append x y = [x] ++ y
 
+head_index x = x !! 0
+
+tail_drop x = drop 1 x
--- Sample.hs
+++ Sample.hs
@@ -10,3 +10,6 @@ myfunc3 e l' = concat.map (\x->if x==e then l' else [x])
 
 myfunc4 e l2 (x:xs) = if x == e then (l2 ++ xs) else [x] ++ check_elem xs
     where check_elem x = undefined
+
+
+myfunc5 x = (x !! 0) + (x !! 2)
