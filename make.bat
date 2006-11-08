83ead72295ba4d0896ee54d5bf8c3d86bf54ad5d
Neil Mitchell <ndmitchell@gmail.com>
2006-11-08 15:12:52 +0000

    Fix the makefile, to see the new location

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-ghc --make -iD:\sources\yhc\11_sep_2006\yhc-devel\src\compiler98 -hidir obj -odir obj -o drhaskell.exe Main 
+ghc --make -iD:\sources\yhc\h.o\yhc\src\libraries\core -iD:\sources\yhc\h.o\yhc\src\libraries\general -hidir obj -odir obj -o drhaskell.exe Main 
