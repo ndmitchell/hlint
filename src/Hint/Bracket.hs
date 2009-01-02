4f1f606163cb9c1bdcd32a5a8ff2f9f488a05503
Neil Mitchell <ndmitchell@gmail.com>
2009-01-02 15:41:34 +0000

    Make the excess bracket detection stuff use HSE.Bracket

--- src/Hint/Bracket.hs
+++ src/Hint/Bracket.hs
@@ -13,47 +13,18 @@ yes = if x then (f y) else z where res = if x then f y else z
 
 module Hint.Bracket where
 
-import Control.Monad
-import Control.Monad.State
-import Data.Generics
+import Control.Arrow
 import Data.Maybe
 import Type
 import HSE.All
 
 
 bracketHint :: Hint
-bracketHint = concatMap bracketExp . universeExp nullSrcLoc
+bracketHint = concatMap bracketExp . children0Exp nullSrcLoc
 
 bracketExp :: (SrcLoc,Exp) -> [Idea]
-bracketExp (loc,x) = [idea "Use fewer brackets" loc x y | Just y <- [f x]]
+bracketExp (loc,x) =
+    [idea "Use fewer brackets" loc x (fromParen x) | isParen x] ++ f loc x
     where
-        f :: Exp -> Maybe Exp
-        f (Paren x) | isAtom x = Just x
-        f x = if cs /= [] && b then Just r else Nothing
-            where
-                (r,(b,[])) = runState (gmapM g x) (False, cs)
-                cs = snd $ precedence x
-        
-        g :: Data a => a -> State (Bool,[Int]) a
-        g x | Just y <- cast x = do
-              (b,c:cs) <- get
-              liftM (fromJust . cast) $ case y of
-                  Paren z | fst (precedence z) < c -> put (True, cs) >> return z
-                  _ -> put (b, cs) >> return y
-        g x = return x
-
-
--- return my precedence, and the precedence of my children
--- higher precedence means no brackets
--- if the object in a position has a lower priority, the brackets are unnecessary
-precedence :: Exp -> (Int,[Int])
-precedence x = case x of
-        If{} -> block * [block,block,block]
-        Let{} -> block * [block]
-        Case{} -> block * [block]
-        InfixApp{} -> op * [op,op]
-        App{} -> appL * [appR,appL]
-        _ -> top * []
-    where
-        (*) = (,)
-        appL:appR:op:block:top:_ = [1..]
+        f loc x = [idea "Use fewer brackets" loc x y | let y = descendBracket (isParen &&& fromParen) x, x /= y] ++
+                  concatMap (uncurry f) (children1Exp loc x)
