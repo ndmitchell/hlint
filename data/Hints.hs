9e0d65bb966d5daf493fa793514e79485b9ef4e8
Neil Mitchell <ndmitchell@gmail.com>
2009-01-02 14:55:44 +0000

    Require brackets around operators, otherwise the interface is tricky

--- data/Hints.hs
+++ data/Hints.hs
@@ -199,7 +199,7 @@ yes = if Debug `elem` opts then ["--debug"] else [] where res = ["--debug" | Deb
 yes = if nullPS s then return False else if headPS s /= '\n' then return False else alter_input tailPS >> return True
     where res = if nullPS s || (headPS s /= '\n') then return False else alter_input tailPS >> return True
 yes = if foo then do stuff; moreStuff; lastOfTheStuff else return ()
-    where res = when foo $ do stuff ; moreStuff ; lastOfTheStuff
+    where res = when foo $ (do stuff ; moreStuff ; lastOfTheStuff)
 </TEST>
 -}
 
