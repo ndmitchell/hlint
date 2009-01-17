1f790f2930a3e57f74ec834377e777fae324767e
Neil Mitchell <ndmitchell@gmail.com>
2009-01-17 13:29:36 +0000

    Remove support for v1 style reports

--- src/Report.hs
+++ src/Report.hs
@@ -12,49 +12,18 @@ import HSE.All
 import Paths_hlint
 
 
-writeReport :: FilePath -> [Idea] -> IO ()
-writeReport file ideas = do
-    writeReport1 file ideas
-    let file2 = dropExtension file ++ "_2" ++ takeExtension file
-    writeReport2 file2 ideas
-
-
-writeReport1 :: FilePath -> [Idea] -> IO ()
-writeReport1 file ideas = writeTemplate "report.html" [("CONTENT",content)] file
-    where
-        content = map f ideas
-        drp = filePrefix $ map (srcFilename . loc) ideas
-        f x = "idea(" ++ concat (intersperse "," args) ++ ");"
-            where args = [show $ show (rank x) ++ ": " ++ hint x
-                         ,show $ drp $ srcFilename $ loc x, show $ srcLine $ loc x, show $ srcColumn $ loc x
-                         ,show $ from x, show $ to x]
-
-
-writeTemplate :: FilePath -> [(String,[String])] -> FilePath -> IO ()
-writeTemplate from content to = do
+writeTemplate :: [(String,[String])] -> FilePath -> IO ()
+writeTemplate content to = do
     dat <- getDataDir
-    src <- readFile $ dat </> from
+    src <- readFile $ dat </> "report.html"
     writeFile to $ unlines $ concatMap f $ lines src
     where
         f ('$':xs) = fromMaybe ['$':xs] $ lookup xs content
         f x = [x]
 
 
-filePrefix :: [FilePath] -> (FilePath -> FilePath)
-filePrefix xs | null xs = flipSlash
-              | otherwise = flipSlash . drop n2
-    where
-        (mn,mx) = (minimum xs, maximum xs)
-        n = length $ takeWhile id $ zipWith (==) mn mx
-        n2 = length $ dropWhile (`notElem` "\\/") $ reverse $ take n mn
-
-        flipSlash = map (\x -> if x == '\\' then '/' else x)
-
-
-
-
-writeReport2 :: FilePath -> [Idea] -> IO ()
-writeReport2 file ideas = writeTemplate "report_2.html" inner file
+writeReport :: FilePath -> [Idea] -> IO ()
+writeReport file ideas = writeTemplate inner file
     where
         generateIds :: [String] -> [(String,Int)] -- sorted by name
         generateIds = map (\x -> (head x, length x)) . group . sort
