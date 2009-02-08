aa0d4c42991979ff73aa15557b5c7388a2fe2cb3
Neil Mitchell <ndmitchell@gmail.com>
2009-02-08 19:38:59 +0000

    Update to the new hs-lint emacs thing

--- data/hs-lint.el
+++ data/hs-lint.el
@@ -4,8 +4,17 @@
 ;;
 ;; Author: Alex Ott <alexott@gmail.com>
 ;; Keywords: haskell, lint, HLint
-;; Requirements: 
+;; Requirements:
 ;; Status: distributed under terms of GPL2 or above
+
+;; Typical message from HLint looks like:
+;;
+;; /Users/ott/projects/lang-exp/haskell/test.hs:52:1: Eta reduce
+;; Found:
+;;   count1 p l = length (filter p l)
+;; Why not:
+;;   count1 p = length . filter p
+
 
 (require 'compile)
 
@@ -24,82 +33,77 @@
   :type 'boolean
   :group 'hs-lint)
 
-(defcustom hs-lint-setup-hook nil
-  "List of hook functions run by `hs-lint-process-setup' (see `run-hooks')."
-  :type 'hook
+(defcustom hs-lint-replace-with-suggestions nil
+  "Replace user's code with suggested replacements"
+  :type 'boolean
   :group 'hs-lint)
 
-(defvar hs-lint-regexp-alist
-  '(("^\\(.+?\\)\\(:[ \t]*\\)\\([0-9]+\\)\\2" 1 3))
-  "Regexp used to match hlint hits.  See `compilation-error-regexp-alist'.")
-
-(defvar hs-lint-menu-map
-  (let ((map (make-sparse-keymap "hs-hints")))
-    (define-key map [stop-subjob]
-      '("Stop HLint" . kill-compilation))
-    (define-key map [hs-lint-mode-separator2]
-      '("----" . nil))
-    (define-key map [hs-lint-first-error]
-      '("First hint" . first-error))
-    (define-key map [hs-lint-previous-error]
-      '("Previous hint" . previous-error))
-    (define-key map [hs-lint-next-error]
-      '("Next hint" . next-error))
-    map))
-
-(defvar hs-lint-mode-map
-  (let ((map (make-sparse-keymap)))
-    ;; Don't inherit from compilation-minor-mode-map,
-    ;; because that introduces a menu bar item we don't want.
-    ;; That confuses C-down-mouse-3.
-    (define-key map [mouse-2] 'compile-goto-error)
-    (define-key map [follow-link] 'mouse-face)
-    (define-key map "\C-c\C-c" 'compile-goto-error)
-    (define-key map "\C-m" 'compile-goto-error)
-    (define-key map "\C-c\C-k" 'kill-compilation)
-    (define-key map "\M-n" 'compilation-next-error)
-    (define-key map "\M-p" 'compilation-previous-error)
-    (define-key map "\M-{" 'compilation-previous-file)
-    (define-key map "\M-}" 'compilation-next-file)
-    (define-key map "\t" 'compilation-next-error)
-    (define-key map [backtab] 'compilation-previous-error)
-		(define-key map "q" 'bury-buffer)
-
-    (define-key map " " 'scroll-up)
-    (define-key map "\^?" 'scroll-down)
-    (define-key map "\C-c\C-f" 'next-error-follow-minor-mode)
-
-    ;; Set up the menu-bar
-    (let ((submap (make-sparse-keymap "HLint")))
-      (define-key map [menu-bar hs-lint]
-				(cons "HLint" submap))
-      (set-keymap-parent submap hs-lint-menu-map))
-    (define-key map [menu-bar hs-lint hs-lint-separator2]
-      '("----" . nil))
-    (define-key map [menu-bar hs-lint hs-lint-recompile]
-      '("Re-run HLint" . recompile))
-    map)
-  "Keymap for HLint log buffers.
-`compilation-minor-mode-map' is a parent of this.")
+(defcustom hs-lint-replace-without-ask nil
+  "Replace user's code with suggested replacements automatically"
+  :type 'boolean
+  :group 'hs-lint)
 
 (defun hs-lint-process-setup ()
   "Setup compilation variables and buffer for `hlint'."
-  (set (make-local-variable 'compilation-exit-message-function)
-       (lambda (status code msg)
-				 (if (eq status 'exit)
-						 (cond ((zerop code)
-										'("finished (code is checked)\n" . "matched"))
-									 ((= code 1)
-										'("Errors during checking of code\n" . "no match"))
-									 (t
-										(cons msg code)))
-					 (cons msg code))))
   (run-hooks 'hs-lint-setup-hook))
 
+;; regex for replace suggestions
+;;
+;; ^\(.*?\):\([0-9]+\):\([0-9]+\): .*
+;; Found:
+;; \s +\(.*\)
+;; Why not:
+;; \s +\(.*\)
+
+(defvar hs-lint-regex
+  "^\\(.*?\\):\\([0-9]+\\):\\([0-9]+\\): .*[\n\C-m]Found:[\n\C-m]\\s +\\(.*\\)[\n\C-m]Why not:[\n\C-m]\\s +\\(.*\\)[\n\C-m]"
+  "Regex for HLint messages")
+
+(defun make-short-string (str maxlen)
+  (if (< (length str) maxlen)
+      str
+    (concat (substring str 0 (- maxlen 3)) "...")))
+
+(defun hs-lint-replace-suggestions ()
+  "Perform actual replacement of suggestions"
+  (goto-char (point-min))
+  (while (re-search-forward hs-lint-regex nil t)
+    (let* ((fname (match-string 1))
+          (fline (string-to-number (match-string 2)))
+          (old-code (match-string 4))
+          (new-code (match-string 5))
+          (msg (concat "Replace '" (make-short-string old-code 30)
+                       "' with '" (make-short-string new-code 30) "'"))
+          (bline 0)
+          (eline 0)
+          (spos 0)
+          (new-old-code ""))
+      (save-excursion
+        (switch-to-buffer (get-file-buffer fname))
+        (goto-line fline)
+        (beginning-of-line)
+        (setf bline (point))
+        (when (or hs-lint-replace-without-ask
+                  (yes-or-no-p msg))
+          (end-of-line)
+          (setf eline (point))
+          (beginning-of-line)
+          (setf old-code (regexp-quote old-code))
+          (while (string-match "\\\\ " old-code spos)
+            (setf new-old-code (concat new-old-code
+                                 (substring old-code spos (match-beginning 0))
+                                 "\\ *"))
+            (setf spos (match-end 0)))
+          (setf new-old-code (concat new-old-code (substring old-code spos)))
+          (remove-text-properties bline eline '(composition nil))
+          (when (re-search-forward new-old-code eline t)
+            (replace-match new-code nil t)))))))
+
 (defun hs-lint-finish-hook (buf msg)
-	"Function, that is executed at the end of HLint execution"
-		(next-error 1 t)
-		)
+  "Function, that is executed at the end of HLint execution"
+  (if hs-lint-replace-with-suggestions
+      (hs-lint-replace-suggestions)
+      (next-error 1 t)))
 
 (define-compilation-mode hs-lint-mode "HLint"
   "Mode for check Haskell source code."
@@ -107,15 +111,16 @@
        'hs-lint-process-setup)
   (set (make-local-variable 'compilation-disable-input) t)
   (set (make-local-variable 'compilation-scroll-output) nil)
-	(set (make-local-variable 'compilation-finish-functions) (list 'hs-lint-finish-hook))
-	)
+  (set (make-local-variable 'compilation-finish-functions)
+       (list 'hs-lint-finish-hook))
+  )
 
 (defun hs-lint ()
-	"Run HLint for current buffer with haskell source"
-	(interactive)
-	(save-some-buffers hs-lint-save-files)
-  (compilation-start (concat hs-lint-command " "
-														 buffer-file-name) 'hs-lint-mode))
+  "Run HLint for current buffer with haskell source"
+  (interactive)
+  (save-some-buffers hs-lint-save-files)
+  (compilation-start (concat hs-lint-command " " buffer-file-name)
+                     'hs-lint-mode))
 
 (provide 'hs-lint)
 ;;; hs-lint.el ends here
