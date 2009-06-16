94a657e36ebb43a6f08732cc9995fb3706f78884
Neil Mitchell <ndmitchell@gmail.com>
2009-06-16 16:20:54 +0000

    Add hints to use if instead of case, fixes bug #191

--- src/Hint/Structure.hs
+++ src/Hint/Structure.hs
@@ -6,6 +6,8 @@
 <TEST>
 yes x y = if a then b else if c then d else e where res = "yes x y\n  | a = b\n  | c = d\n  | otherwise = e"
 no x y = if a then b else c
+yes x = case x of {True -> a ; False -> b} where res = "if x then a else b"
+yes x = case x of {False -> a ; _ -> b} where res = "if x then b else a"
 </TEST>
 -}
 
@@ -20,8 +22,8 @@ import Data.Maybe
 
 
 structureHint :: Hint
-structureHint (FunBind xs) = concatMap useGuards xs
-structureHint _ = []
+structureHint x = concat [concatMap useGuards xs | FunBind xs <- [x]] ++
+                  concatMap useIf (universeExp nullSrcLoc x)
 
 
 useGuards :: Match -> [Idea]
@@ -37,3 +39,16 @@ asGuards :: Exp -> [GuardedRhs]
 asGuards (Paren x) = asGuards x
 asGuards (If a b c) = GuardedRhs nullSrcLoc [Qualifier a] b : asGuards c
 asGuards x = [GuardedRhs nullSrcLoc [Qualifier $ toNamed "otherwise"] x]
+
+
+useIf :: (SrcLoc,Exp) -> [Idea]
+useIf (loc,x@(Case on [simpAlt -> Just (as,av), simpAlt -> Just (bs,bv)]))
+    | as == "True"  && bs `elem` ["False","_"] = iff av bv
+    | as == "False" && bs `elem` ["True" ,"_"] = iff bv av
+    where
+        iff t f = [warn "Use if" loc x (If on t f)]
+useIf _ = []
+
+simpAlt (Alt _ p (UnGuardedAlt x) (BDecls [])) = Just (prettyPrint p, x)
+simpAlt _ = Nothing
+
