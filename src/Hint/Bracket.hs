e7a6cc24286e42041f48d5eb4a31de2afc756be1
Neil Mitchell <ndmitchell@gmail.com>
2009-07-02 10:21:52 +0000

    Fix bug #203

--- src/Hint/Bracket.hs
+++ src/Hint/Bracket.hs
@@ -15,6 +15,8 @@ yes = white $ keysymbol where res = white keysymbol
 yes = operator foo $ operator where res = operator foo operator
 no = operator foo $ operator bar
 yes = (b $ c d) ++ e where res = b (c d) ++ e
+yes = (a b $ c d) ++ e where res = a b (c d) ++ e
+no = (f . g $ a) ++ e
 </TEST>
 -}
 
@@ -40,8 +42,15 @@ bracketExp (loc,x) =
 
         testDollar loc x =
             [idea Error "Redundant $" loc x y | InfixApp a d b <- [x], opExp d ~= "$"
-            ,let y = App a b, not $ needBracket 0 y a, not $ needBracket 1 y b] ++
-            [idea Error "Redundant $" loc x y | InfixApp a op b <- [x]
-            ,(first,Paren (InfixApp c1 d c2)) <- [(True,a),(False,b)], opExp d ~= "$"
-            ,let c = App c1 (Paren c2),let (a2,b2) = if first then (c,b) else (a,c)
-            ,let y = InfixApp a2 op b2]
+            ,let y = App a b, not $ needBracket 0 y a, not $ needBracket 1 y b]
+            ++
+            [idea Error "Redundant $" loc x (t y)
+            |(t, Paren (InfixApp a1 op1 a2)) <- infixes x
+            ,opExp op1 ~= "$", isVar a1 || isApp a1 || isParen a1, not $ isAtom a2
+            ,let y = App a1 (Paren a2)]
+
+
+-- return both sides, and a way to put them together again
+infixes :: Exp -> [(Exp -> Exp, Exp)]
+infixes (InfixApp a b c) = [(InfixApp a b, c), (\a -> InfixApp a b c, a)]
+infixes _ = []
