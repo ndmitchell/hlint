62eecfb7fb038c0d39a71232550528b7b4df5678
Neil Mitchell <ndmitchell@gmail.com>
2009-09-05 18:21:23 +0000

    Add a script to automate HPC checking

---
+++ hpc.bat
@@ -0,0 +1,10 @@
+mkdir .hpc
+ghc --make -isrc -i. src\Main.hs -w -fhpc -odir .hpc -hidir .hpc -threaded -o .hpc\hlint-test
+del hlint-test.tix
+.hpc\hlint-test --help
+.hpc\hlint-test --test
+.hpc\hlint-test src --report=.hpc\report.html +RTS -N2
+hpc.exe markup hlint-test.tix --destdir=.hpc
+hpc.exe report hlint-test.tix
+del hlint-test.tix
+start .hpc\hpc_index_fun.html
