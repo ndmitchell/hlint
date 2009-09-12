8a8dbc0fef0effb9792cf404d7cfb1d9d42af33f
Neil Mitchell <ndmitchell@gmail.com>
2009-09-12 16:47:28 +0000

    Don't suggest eta reduction on - or +

--- src/Hint/Lambda.hs
+++ src/Hint/Lambda.hs
@@ -31,6 +31,8 @@ yes = foo (\x l -> sum x x l) -- \x -> sum x x
 test = foo (\x -> y == x) -- (y ==)
 test = foo (\x -> x == g y) -- (== g y)
 test = foo (\x -> g x == x)
+cp i = show (i - 1)
+cp i = show (i + 1)
 </TEST>
 -}
 
@@ -96,8 +98,9 @@ etaReduces ps x | ps /= [], PVar p <- last ps, p /= Ident "mr", Just y <- etaRed
 
 etaReduce :: Name -> Exp -> Maybe Exp
 etaReduce x (App y (Var (UnQual z))) | x == z && x `notElem` universeBi y = Just y
-etaReduce x (InfixApp y op z) | var x == y && x `notElem` universeBi z = Just $ RightSection op z
-                              | var x == z && x `notElem` universeBi y = Just $ LeftSection y op
+etaReduce x (InfixApp y op z) | goodOp && var x == y && x `notElem` universeBi z = Just $ RightSection op z
+                              | goodOp && var x == z && x `notElem` universeBi y = Just $ LeftSection y op
+    where goodOp = op `notElem` map (QVarOp . UnQual . Symbol . return) "+-"
 etaReduce x (App y z) | not (uglyEta y z) && x `notElem` universeBi y = do
     z2 <- etaReduce x z
     return $ InfixApp y (QVarOp $ UnQual $ Symbol ".") z2
