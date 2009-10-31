5e84f30b2978e2672c16bfa6bc9930c60f7510f9
Neil Mitchell <ndmitchell@gmail.com>
2009-10-31 19:35:25 +0000

    Run more bits when generating HPC stats

--- hpc.bat
+++ hpc.bat
@@ -3,6 +3,7 @@ ghc --make -isrc -i. src\Main.hs -w -fhpc -odir .hpc -hidir .hpc -threaded -o .h
 del hlint-test.tix
 .hpc\hlint-test --help
 .hpc\hlint-test --test
+.hpc\hlint-test src --report=.hpc\hlint-test-report.html +RTS -N3
 hpc.exe markup hlint-test.tix --destdir=.hpc
 hpc.exe report hlint-test.tix
 del hlint-test.tix
