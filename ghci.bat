037c5f705eace2eadd17e44cc6b2eb26e018c588
Neil Mitchell <ndmitchell@gmail.com>
2009-11-05 19:04:00 +0000

    Upgrade to HSE 1.3.*, add to the changelog and run script

--- ghci.bat
+++ ghci.bat
@@ -1,1 +1,2 @@
-ghci.exe -isrc -i. src\Main.hs -fno-warn-overlapping-patterns -w -fwarn-unused-binds -fwarn-unused-imports
+ghci.exe -isrc -i. src\Main.hs -fno-warn-overlapping-patterns -w -fwarn-unused-binds -fwarn-unused-imports -package haskell-src-exts-1.3.0
+
