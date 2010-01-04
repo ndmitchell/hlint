b0588575c86fd9a3e39b9378de957bacdfc91e3f
Neil Mitchell <ndmitchell@gmail.com>
2010-01-04 08:06:03 +0000

    Remove the restriction to haskell-src-exts-1.3.0 in the .bat file

--- ghci.bat
+++ ghci.bat
@@ -1,2 +1,2 @@
-ghci.exe -isrc -i. src\Main.hs -fno-warn-overlapping-patterns -w -fwarn-unused-binds -fwarn-unused-imports -package haskell-src-exts-1.3.0
+ghci.exe -isrc -i. src\Main.hs -fno-warn-overlapping-patterns -w -fwarn-unused-binds -fwarn-unused-imports
 
