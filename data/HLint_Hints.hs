a739bf3865ab1cc59051223bed2198ea4705f6dd
Neil Mitchell <ndmitchell@gmail.com>
2010-01-20 09:26:45 +0000

    Add some more hints to the HLint hint set

--- data/HLint_Hints.hs
+++ data/HLint_Hints.hs
@@ -7,6 +7,8 @@ error = QVarOp a (UnQual b x) ==> toNamed x
 error = QConOp a (UnQual b x) ==> toNamed x
 error = UnQual a (Symbol b x) ==> toNamed x
 error = UnQual a (Ident  b x) ==> toNamed x
+error = idea Error ==> err
+error = idea Warning ==> warn
 
 -- deliberately creates an Ident with "...", which should be a symbol
 ignore "Use toNamed" = Hint.Naming.shorten
