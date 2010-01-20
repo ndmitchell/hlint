91e4fbc8db9fc0ad5073f01be36252a68301c904
Neil Mitchell <ndmitchell@gmail.com>
2010-01-20 09:27:54 +0000

    Rewrite the bracketing hints, give errors for severe bracket warnings

--- src/Hint/Bracket.hs
+++ src/Hint/Bracket.hs
@@ -1,30 +1,37 @@
 {-
+Raise an error if you are bracketing an atom, or are enclosed be a list bracket
+
 <TEST>
+-- bracket reduction
 yes = (f x) x -- @Warning f x x
 no = f (x x)
-yes = (foo) -- foo
-yes = (foo bar) -- foo bar
+yes = (foo) -- @Error foo
+yes = (foo bar) -- @Error foo bar
+yes = foo (bar) -- @Error bar
+yes = foo ((x x)) -- @Error (x x)
 yes = (f x) ||| y -- @Warning f x ||| y
 yes = if (f x) then y else z -- @Warning if f x then y else z
 yes = if x then (f y) else z -- @Warning if x then f y else z
 yes = (a foo) :: Int -- @Warning a foo :: Int
+yes = [(foo bar)] -- @Error [foo bar]
 
+-- dollar reduction tests
 no = groupFsts . sortFst $ mr
 yes = split "to" $ names -- split "to" names
 yes = white $ keysymbol -- white keysymbol
 yes = operator foo $ operator -- operator foo operator
 no = operator foo $ operator bar
+
+-- $/bracket rotation tests
 yes = (b $ c d) ++ e -- b (c d) ++ e
 yes = (a b $ c d) ++ e -- a b (c d) ++ e
 no = (f . g $ a) ++ e
-yes = [(foo bar)] -- [foo bar]
 </TEST>
 -}
 
 
 module Hint.Bracket where
 
-import Control.Arrow
 import Type
 import Hint
 import HSE.All
@@ -33,23 +40,36 @@ import HSE.All
 bracketHint :: DeclHint
 bracketHint _ _ = concatMap bracketExp . childrenBi
 
+
+msgBracket = "Redundant bracket"
+msgDollar = "Redundant $"
+
+
 bracketExp :: Exp_ -> [Idea]
-bracketExp x =
-    [g x (fromParen x) | isParen x] ++ f x
+bracketExp o@(Paren _ x) = err msgBracket o x : bracketExp x
+bracketExp x = bracket x ++ dollar x
+
+
+bracket :: Exp_ -> [Idea]
+bracket o@(List _ [Paren _ x]) = err msgBracket o (List an [x]) : bracket x
+bracket o@(Paren _ x) | isAtom x = err msgBracket o x : bracket x
+bracket o = concat $ zipWith f [0..] $ holes o
+    where f i (o2@(Paren _ x),gen)
+                | isAtom x = err msgBracket o2 x : bracket x
+                | not $ needBracket i o x = warn msgBracket o (gen x) : bracket x
+          f i (x,_) = bracket x
+
+
+dollar :: Exp_ -> [Idea]
+dollar = concatMap f . universe
     where
-        f x = testDirect x ++ testDollar x ++ concatMap f (children x)
-        g = warn "Redundant brackets"
-
-        testDirect x = [g x y | let y = descendBracket (isParen &&& fromParen) x, x /=~= y]
-
-        testDollar x =
-            [idea Error "Redundant $" x y | InfixApp _ a d b <- [x], opExp d ~= "$"
-            ,let y = App an a b, not $ needBracket 0 y a, not $ needBracket 1 y b]
-            ++
-            [idea Error "Redundant $" x (t y)
-            |(t, Paren _ (InfixApp _ a1 op1 a2)) <- infixes x
-            ,opExp op1 ~= "$", isVar a1 || isApp a1 || isParen a1, not $ isAtom a2
-            ,let y = App an a1 (Paren an a2)]
+        f x = [warn msgDollar x y | InfixApp _ a d b <- [x], opExp d ~= "$"
+              ,let y = App an a b, not $ needBracket 0 y a, not $ needBracket 1 y b]
+              ++
+              [warn msgDollar x (t y)
+              |(t, Paren _ (InfixApp _ a1 op1 a2)) <- infixes x
+              ,opExp op1 ~= "$", isVar a1 || isApp a1 || isParen a1, not $ isAtom a2
+              ,let y = App an a1 (Paren an a2)]
 
 
 -- return both sides, and a way to put them together again
