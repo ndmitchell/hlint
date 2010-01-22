ba4e13e13aeef04e876b7633b1c3b1310119f53b
Neil Mitchell <ndmitchell@gmail.com>
2010-01-22 09:51:07 +0000

    Improve the shell eq test, now uses SYB to do the constructor equality test

--- src/HSE/Eq.hs
+++ src/HSE/Eq.hs
@@ -4,16 +4,12 @@ module HSE.Eq(eqExpShell) where
 import HSE.Type
 import HSE.Util
 import Data.Function
+import Data.Data
 
 
 eqExpShell :: Exp_ -> Exp_ -> Bool
 eqExpShell x y = eqExpShellFast x y && eqExpShellSlow x y
 
-
-eqExpShellFast x y = f (show x) (show y)
-    where f (x:xs) (y:ys) | x == y = x == ' ' || f xs ys
-          f [] [] = True
-          f _ _ = False
-
+eqExpShellFast x y = toConstr x == toConstr y
 
 eqExpShellSlow = (==) `on` descend (const $ Do an [])
