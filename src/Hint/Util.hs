f3ba424bc4c397f115040a4607a9b700d9533aa9
Neil Mitchell <ndmitchell@gmail.com>
2010-01-29 19:38:34 +0000

    Fix some incorrect niceLambda transformations

--- src/Hint/Util.hs
+++ src/Hint/Util.hs
@@ -10,13 +10,13 @@ niceLambda :: [String] -> Exp_ -> Exp_
 niceLambda xs (Paren _ x) = niceLambda xs x
 niceLambda xs (Lambda _ ((view -> PVar_ v):vs) x) = niceLambda (xs++[v]) (Lambda an vs x)
 niceLambda xs (Lambda _ [] x) = niceLambda xs x
-niceLambda [x] (App _ a (view -> Var_ b)) | x == b = a
+niceLambda [x] (App _ a (view -> Var_ b)) | x == b, x `notElem` vars a = a
 niceLambda [x] (App _ a (Paren _ (App _ b (view -> Var_ c))))
-    | isAtom a && isAtom b && x == c
+    | isAtom a && isAtom b && x == c && x `notElem` (vars a ++ vars b)
     = if a ~= "$" then LeftSection an b (toNamed "$") else InfixApp an a (toNamed ".") b
 niceLambda [x] (InfixApp _ a op b)
-    | view a == Var_ x = RightSection an op b
-    | view b == Var_ x = LeftSection an a op
+    | view a == Var_ x, x `notElem` vars b, allowRightSection (fromNamed op) = RightSection an op b
+    | view b == Var_ x, x `notElem` vars a, allowLeftSection  (fromNamed op) = LeftSection an a op
 niceLambda [x,y] (view -> App2 op (view -> Var_ x1) (view -> Var_ y1))
     | x1 == x && y1 == y = op
     | x1 == y && y1 == x = App an (toNamed "flip") op
