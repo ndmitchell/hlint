d38137a6d2e45935f5342d051b2faa6489d6ceb0
Neil Mitchell <ndmitchell@gmail.com>
2010-01-29 19:38:54 +0000

    Don't suggest flip unless the function is atomic

--- data/Default.hs
+++ data/Default.hs
@@ -82,7 +82,7 @@ error = (\(_,y) -> y) ==> snd
 error = (\(x,_) -> x) ==> fst
 error = (\x y-> f (x,y)) ==> curry f where _ = notIn [x,y] f
 error = (\(x,y) -> f x y) ==> uncurry f where _ = notIn [x,y] f
-warn  = (\x -> f x y) ==> flip f y where _ = notIn x [f,y]
+warn  = (\x -> f x y) ==> flip f y where _ = notIn x [f,y] && isAtom f
 error = (($) . f) ==> (f $)
 warn  = (\x -> y) ==> const y where _ = isAtom y && notIn x y
 error "Redundant flip" = flip f x y ==> f y x where _ = isApp original
