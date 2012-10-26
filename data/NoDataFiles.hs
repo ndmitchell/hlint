65d3ac1d4e5b2bea86d7bf5336430d9592a078b7
Neil Mitchell <ndmitchell@gmail.com>
2012-10-26 10:33:35 +0000

    Add an example for how to use if you don't want data files

---
+++ data/NoDataFiles.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE TemplateHaskell, ViewPatterns #-}
+
+-- This script shows how to use HLint without any data files (provided you do not use --report)
+-- Running this script should report two errors
+
+import Data.List
+import Data.List
+import Language.Haskell.HLint
+import Language.Haskell.TH.Syntax
+import Network.HTTP
+
+
+hints :: String
+hints = $(do
+    let openURL x = getResponseBody =<< simpleHTTP (getRequest x)
+    let grab x = qRunIO $ fmap lines $ openURL $ "http://community.haskell.org/~ndm/darcs/hlint/data/" ++ x
+    outer <- grab "HLint.hs"
+    hints <- grab "Default.hs"
+    lift $ unlines $
+        filter (not . isSuffixOf "HLint.Default") outer ++
+        filter (not . isPrefixOf "module") hints
+    )
+
+main :: IO ()
+main = do
+    x <- hlint $ ["NoDataFiles.hs", "--quiet", "--with=" ++ hints]
+    putStr $ unlines $ map show x
