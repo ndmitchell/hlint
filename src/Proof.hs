833fe17e30dc597b8521d83d4f6ed4fed9ee6afe
Neil Mitchell <ndmitchell@gmail.com>
2013-01-15 17:45:07 +0000

    Add a few more infix/prefix operators

--- src/Proof.hs
+++ src/Proof.hs
@@ -126,8 +126,8 @@ hintTheorems xs =
         loc (SrcSpanInfo (SrcSpan file ln _ _ _) _) = takeFileName file ++ ":" ++ show ln
 
         subs xs = flip lookup [(reverse b, reverse a) | x <- words xs, let (a,'=':b) = break (== '=') $ reverse x]
-        funs = subs "id=ID not=neg or=the_or and=the_and (||)=tror (&&)=trand (++)=append"
-        ops = subs "||=orelse &&=andalso .=oo ===eq /==neq"
+        funs = subs "id=ID not=neg or=the_or and=the_and (||)=tror (&&)=trand (++)=append (==)=eq (/=)=neq"
+        ops = subs "||=orelse &&=andalso .=oo ===eq /==neq ++=++ !!=!!"
         pre = flip elem $ words "eq neq"
         cons = subs "True=TT False=FF"
 
