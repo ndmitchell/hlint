44c5888ab8303b4542e738440d5228852d3659e1
Neil Mitchell <ndmitchell@gmail.com>
2013-07-23 12:00:24 +0100

    Fix up the tests for a removed hint

--- data/Default.hs
+++ data/Default.hs
@@ -539,7 +539,6 @@ yes = [v | v <- xs] -- xs
 no  = [Left x | Left x <- xs]
 yes = Map.union a b -- a `Map.union` b
 when p s = if p then s else return ()
-yes = x ^^ 18 -- x ** 18
 no = x ^^ 18.5
 instance Arrow (->) where first f = f *** id
 yes = fromInteger 12 -- 12
