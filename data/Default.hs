b41a5ba7c15eb6f78f58d88ed29b4b8ffe59f607
Neil Mitchell <ndmitchell@gmail.com>
2013-07-23 09:35:26 +0100

    Comment out a test about isPrefixOf, since it got deleted

--- data/Default.hs
+++ data/Default.hs
@@ -494,7 +494,6 @@ no  = if a then 1 else if b then 3 else 2
 yes = a >>= return . id -- Control.Monad.liftM id a
 yes = (x !! 0) + (x !! 2) -- head x
 yes = if b < 42 then [a] else [] -- [a | b < 42]
-yes = take 5 (foo xs) == "hello" -- "hello" `Data.List.isPrefixOf` foo xs
 no  = take n (foo xs) == "hello"
 yes = head (reverse xs) -- last xs
 yes = reverse xs `isPrefixOf` reverse ys -- isSuffixOf xs ys
