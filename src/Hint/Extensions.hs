ae00a6b1802e4ca716f7e6da76c560deea647a69
Neil Mitchell <neil.mitchell@sc.com>
2013-08-22 22:44:09 +0100

    Add a concatUnzip utility function

--- src/Hint/Extensions.hs
+++ src/Hint/Extensions.hs
@@ -148,7 +148,7 @@ hasDerive nt want m = not $ null $ intersect want $ if nt then new ++ dat else d
 -- | What is derived on newtype, and on data type
 --   'deriving' declarations may be on either, so we approximate
 derives :: Module_ -> ([String],[String])
-derives = (concat *** concat) . unzip . map f . childrenBi
+derives = concatUnzip . map f . childrenBi
     where
         f :: Decl_ -> ([String], [String])
         f (DataDecl _ dn _ _ _ ds) = g dn ds
