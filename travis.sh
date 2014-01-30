30482458353f5fa56aff19a7b93126bc9ad51a8c
Neil Mitchell <ndmitchell@gmail.com>
2014-01-30 21:39:44 +0000

    Attempt at simplifying the travis script

--- travis.sh
+++ travis.sh
@@ -1,16 +1,3 @@
 #!/bin/bash -e -x
-rm -f dist/*.tar.gz
-rm -rf dist/snapshot
-cabal configure --ghc-options=-Werror
-cabal sdist
-cd dist
-mkdir snapshot
-tar -xf *.tar.gz -C snapshot
-cd snapshot
-mv hlint-* hlint # predictable name
-cd hlint
-cabal configure --ghc-options=-Werror
-cabal build
-cd ../../..
-export PATH=/home/travis/.ghc-multi/7.6.3/bin:$PATH
-hlint_datadir=data dist/snapshot/hlint/dist/build/hlint/hlint --test
+wget https://raw.github.com/ndmitchell/neil/master/travis.sh -O - --no-check-certificate --no-cache --quiet | sh
+hlint_datadir=data hlint --test
