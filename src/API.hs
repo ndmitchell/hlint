688c5612602b531fa5ad00be84c70c40bfc3c19c
Neil Mitchell <ndmitchell@gmail.com>
2014-01-31 09:27:08 +0000

    Add a new module which will become the official API

---
+++ src/API.hs
@@ -0,0 +1,27 @@
+
+-- | This module represents the evolving API of HLint, it is not yet stable, complete or usable
+module API(
+    -- * Settings
+    module Settings,
+    -- * Idea
+    module Idea,
+    -- * Apply
+    module Apply,
+    -- * Hint.Type
+    Hint(..),
+    -- * Hint.All
+    staticHints, dynamicHints,
+    -- * HSE.NameMatch
+    Scope, moduleScope,
+    NameMatch, nameMatch, nameQualify,
+    -- * HSE
+    Module_, Exp_, S
+    ) where
+
+import Settings
+import Idea
+import Apply
+import Hint.Type hiding (Module_, Exp_, S)
+import Hint.All
+import HSE.All
+
