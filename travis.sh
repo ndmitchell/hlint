a2c1b3d4012ca4073f08d6079c82ec0598eab435
Neil Mitchell <ndmitchell@gmail.com>
2014-03-17 11:08:09 +0000

    Fix the test running script

--- travis.sh
+++ travis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e -x
 wget https://raw.github.com/ndmitchell/neil/master/travis.sh -O - --no-check-certificate --no-cache --quiet | sh
 cabal install
-hlint_datadir=data hlint --test
+hlint_datadir=data hlint test
