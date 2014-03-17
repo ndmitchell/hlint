4dda414ccb4dc60143aa820d64926b685d08ea9e
Neil Mitchell <ndmitchell@gmail.com>
2014-03-17 18:31:35 +0000

    Add handling of parse errors when running grep

--- src/Grep.hs
+++ src/Grep.hs
@@ -5,14 +5,25 @@ module Grep(runGrep) where
 import Language.Haskell.HLint2
 import HSE.All
 import Control.Monad
+import Data.List
+import Util
 
 
 runGrep :: String -> ParseFlags -> [FilePath] -> IO ()
 runGrep pattern flags files = do
-    let exp = fromParseResult $ parseExp pattern
+    exp <- case parseExp pattern of
+        ParseOk x -> return x
+        ParseFailed sl msg ->
+            exitMessage $ (if "Parse error" `isPrefixOf` msg then msg else "Parse error in pattern: " ++ msg) ++ "\n" ++
+                          pattern ++ "\n" ++
+                          replicate (srcColumn sl - 1) ' ' ++ "^"
     let scope = scopeCreate $ Module an Nothing [] [] []
     let rule = hintRules [HintRule Warning "grep" scope exp (Tuple an Boxed []) Nothing []]
     forM_ files $ \file -> do
-        Right m <- parseModuleEx flags file Nothing
-        forM_ (applyHints [] rule [m]) $ \Idea{..} -> do
-            putStr $ unlines $ showSrcLoc (getPointLoc ideaSpan) : map ("  "++) (lines ideaFrom)
+        res <- parseModuleEx flags file Nothing
+        case res of
+            Left (ParseError sl msg ctxt) -> do
+                putStrLn $ showSrcLoc sl ++ ": " ++ (if "Parse error" `isPrefixOf` msg then msg else "Parse error: " ++ msg) ++ "\n" ++ ctxt
+            Right m ->
+                forM_ (applyHints [] rule [m]) $ \Idea{..} -> do
+                    putStrLn $ unlines $ (showSrcLoc (getPointLoc ideaSpan) ++ ":") : map ("  "++) (lines ideaFrom)
