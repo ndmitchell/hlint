45bb6b92fba0f5568ae34b2a353e8bab9bbd80cb
Neil Mitchell <ndmitchell@gmail.com>
2014-03-20 15:02:21 +0000

    Avoid using the time binary

--- travis.hs
+++ travis.hs
@@ -4,7 +4,7 @@ import Neil
 main :: IO ()
 main = do
     cmd "hlint test"
-    (time,_) <- duration $ cmdOut "time hlint src"
+    (time,_) <- duration $ cmdOut "hlint src"
     print $ "Running HLint on self took " ++ show time ++ "s"
     cmd "ghc -threaded -rtsopts -isrc -i. src/Paths.hs src/Main.hs --make -O -prof -auto-all -caf-all"
     cmdOut "src/Main src +RTS -p"
