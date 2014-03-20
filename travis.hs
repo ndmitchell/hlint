8b14ff989eea1045feeff545b928ae869c8bec93
Neil Mitchell <ndmitchell@gmail.com>
2014-03-20 15:46:22 +0000

    Try a different location for Main.prof

--- travis.hs
+++ travis.hs
@@ -8,4 +8,4 @@ main = do
     print $ "Running HLint on self took " ++ show time ++ "s"
     cmd "ghc -threaded -rtsopts -isrc -i. src/Paths.hs src/Main.hs --make -O -prof -auto-all -caf-all"
     cmdCode "src/Main src +RTS -p"
-    cmd "cat src/Main.prof"
+    cmd "cat Main.prof"
