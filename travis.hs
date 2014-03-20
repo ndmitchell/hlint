9eaec0453d01c43d8553b5fd99b76f22e939c161
Neil Mitchell <ndmitchell@gmail.com>
2014-03-20 15:28:46 +0000

    Use cmdCode, not cmdOut, for the travis script

--- travis.hs
+++ travis.hs
@@ -4,8 +4,8 @@ import Neil
 main :: IO ()
 main = do
     cmd "hlint test"
-    (time,_) <- duration $ cmdOut "hlint src"
+    (time,_) <- duration $ cmdCode "hlint src"
     print $ "Running HLint on self took " ++ show time ++ "s"
     cmd "ghc -threaded -rtsopts -isrc -i. src/Paths.hs src/Main.hs --make -O -prof -auto-all -caf-all"
-    cmdOut "src/Main src +RTS -p"
+    cmdCode "src/Main src +RTS -p"
     cmd "cat src/Main.prof"
