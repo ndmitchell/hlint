93a9a043c6f1ea87bb67a3d9f435d4b22735acc0
Neil Mitchell <ndmitchell@gmail.com>
2014-03-20 22:22:54 +0000

    Switch a print for a putStrLn in the travis script

--- travis.hs
+++ travis.hs
@@ -5,7 +5,7 @@ main :: IO ()
 main = do
     cmd "hlint test"
     (time,_) <- duration $ cmdCode "hlint src"
-    print $ "Running HLint on self took " ++ show time ++ "s"
+    putStrLn $ "Running HLint on self took " ++ show time ++ "s"
     cmd "ghc -threaded -rtsopts -isrc -i. src/Paths.hs src/Main.hs --make -O -prof -auto-all -caf-all"
     cmdCode "src/Main src +RTS -p"
     cmd "head -n32 Main.prof"
