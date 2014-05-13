bf0999eb2cd5986a310cfeed1d56a6ad951db124
Neil Mitchell <ndmitchell@gmail.com>
2014-05-13 18:34:45 +0100

    Change translate to check all files in one go, faster

--- src/Test/All.hs
+++ src/Test/All.hs
@@ -37,7 +37,7 @@ test CmdTest{..} main dataDir files = withBuffering stdout NoBuffering $ withTes
     wrap "Hint names" $ mapM_ (\x -> do progress; testNames $ snd x) testFiles
     wrap "Hint annotations" $ forM_ testFiles $ \(file,h) -> do progress; testAnnotations h file
     when cmdTypeCheck $ wrap "Hint typechecking" $
-        forM_ testFiles $ \(file,h) -> when (takeFileName file /= "Test.hs") $ do progress; testTypeCheck h
+        progress >> testTypeCheck [h | (file, h) <- testFiles, takeFileName file /= "Test.hs"]
 
     when (null files && not hasSrc) $ putStrLn "Warning, couldn't find source code, so non-hint tests skipped"
 
--- src/Test/Translate.hs
+++ src/Test/Translate.hs
@@ -3,10 +3,12 @@
 -- | Translate the hints to Haskell and run with GHC.
 module Test.Translate(testTypeCheck) where
 
+import Control.Monad
 import Data.List
 import Data.Maybe
 import System.Cmd
 import System.Exit
+import System.FilePath
 
 import Settings
 import Util
@@ -14,12 +16,44 @@ import HSE.All
 import Test.Util
 
 
+replace :: String -> String -> String -> String
+replace from to xs | Just xs <- stripPrefix from xs = to ++ replace from to xs
+replace from to (x:xs) = x : replace from to xs
+replace from to [] = []
+
+
+runMains :: [String] -> IO ()
+runMains xs = withTemporaryFiles "HLint_tmp.hs" (length xs + 1) $ \(root:bodies) -> do
+    forM_ (zip bodies xs) $ \(file,x) -> do
+        writeFile file $ replace "module Main" ("module " ++ takeBaseName file) x
+    let ms = map takeBaseName bodies
+    writeFile root $ unlines $
+        ["import qualified " ++ m | m <- ms] ++
+        ["main = do"] ++
+        ["    " ++ m ++ ".main" | m <- ms]
+    res <- system $ "runhaskell -i" ++ takeDirectory root ++ " " ++ root
+    replicateM_ (length xs) $ tested $ res == ExitSuccess
+
+
 -- | Given a set of hints, do all the HintRule hints type check
-testTypeCheck :: [Setting] -> IO ()
-testTypeCheck hints = withTemporaryFile "hlinttmp.hs" $ \file -> do
-        writeFile file $ unlines contents
-        res <- system $ "runhaskell " ++ file
-        tested $ res == ExitSuccess
+testTypeCheck :: [[Setting]] -> IO ()
+testTypeCheck hints = runMains $ map (unlines . toTypeCheck) hints
+
+toTypeCheck :: [Setting] -> [String]
+toTypeCheck hints =
+        ["{-# LANGUAGE NoMonomorphismRestriction, ExtendedDefaultRules #-}"
+        ,"module Main(main) where"] ++
+        concat [map (prettyPrint . hackImport) $ scopeImports $ hintRuleScope x | x <- take 1 matches] ++
+        ["main = return ()"
+        ,"(==>) :: a -> a -> a; (==>) = undefined"
+        ,"_noParen_ = id"
+        ,"_eval_ = id"] ++
+        ["{-# LINE " ++ show (startLine $ ann rhs) ++ " " ++ show (fileName $ ann rhs) ++ " #-}\n" ++
+         prettyPrint (PatBind an (toNamed $ "test" ++ show i) Nothing bod Nothing)
+        | (i, HintRule _ _ _ lhs rhs side _) <- zip [1..] matches, "notTypeSafe" `notElem` vars (maybeToList side)
+        , let vs = map toNamed $ nub $ filter isUnifyVar $ vars lhs ++ vars rhs
+        , let inner = InfixApp an (Paren an lhs) (toNamed "==>") (Paren an rhs)
+        , let bod = UnGuardedRhs an $ if null vs then inner else Lambda an vs inner]
     where
         matches = [x | SettingMatchExp x <- hints]
 
@@ -27,17 +61,3 @@ testTypeCheck hints = withTemporaryFile "hlinttmp.hs" $ \file -> do
         hackImport i@ImportDecl{importAs=Just a,importModule=b}
             | prettyPrint b `elem` words "Maybe List Monad IO Char" = i{importAs=Just b,importModule=a}
         hackImport i = i
-
-        contents =
-            ["{-# LANGUAGE NoMonomorphismRestriction, ExtendedDefaultRules #-}"] ++
-            concat [map (prettyPrint . hackImport) $ scopeImports $ hintRuleScope x | x <- take 1 matches] ++
-            ["main = return ()"
-            ,"(==>) :: a -> a -> a; (==>) = undefined"
-            ,"_noParen_ = id"
-            ,"_eval_ = id"] ++
-            ["{-# LINE " ++ show (startLine $ ann rhs) ++ " " ++ show (fileName $ ann rhs) ++ " #-}\n" ++
-             prettyPrint (PatBind an (toNamed $ "test" ++ show i) Nothing bod Nothing)
-            | (i, HintRule _ _ _ lhs rhs side _) <- zip [1..] matches, "notTypeSafe" `notElem` vars (maybeToList side)
-            , let vs = map toNamed $ nub $ filter isUnifyVar $ vars lhs ++ vars rhs
-            , let inner = InfixApp an (Paren an lhs) (toNamed "==>") (Paren an rhs)
-            , let bod = UnGuardedRhs an $ if null vs then inner else Lambda an vs inner]
