75b209d819824f813266a5121a4eefd0d3164a80
Neil Mitchell <ndmitchell@gmail.com>
2014-05-13 07:46:36 +0100

    Split the typecheck translation hint into a separate module

---
+++ src/Test/Translate.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE PatternGuards, ScopedTypeVariables, RecordWildCards, ViewPatterns #-}
+
+module Test.Translate(typeCheckHints) where
+
+import Control.Exception
+import Data.List
+import Data.Maybe
+import System.Directory
+import System.IO
+import System.Cmd
+import System.Exit
+
+import Settings
+import HSE.All
+import Test.Util
+
+
+-- | Given a set of hints, do all the HintRule hints type check
+typeCheckHints :: [Setting] -> IO ()
+typeCheckHints hints = bracket
+    (openTempFile "." "hlinttmp.hs")
+    (\(file,h) -> removeFile file)
+    $ \(file,h) -> do
+        hPutStrLn h $ unlines contents
+        hClose h
+        res <- system $ "runhaskell " ++ file
+        progress
+        tested $ res == ExitSuccess
+    where
+        matches = [x | SettingMatchExp x <- hints]
+
+        -- Hack around haskell98 not being compatible with base anymore
+        hackImport i@ImportDecl{importAs=Just a,importModule=b}
+            | prettyPrint b `elem` words "Maybe List Monad IO Char" = i{importAs=Just b,importModule=a}
+        hackImport i = i
+
+        contents =
+            ["{-# LANGUAGE NoMonomorphismRestriction, ExtendedDefaultRules #-}"] ++
+            concat [map (prettyPrint . hackImport) $ scopeImports $ hintRuleScope x | x <- take 1 matches] ++
+            ["main = return ()"
+            ,"(==>) :: a -> a -> a; (==>) = undefined"
+            ,"_noParen_ = id"
+            ,"_eval_ = id"] ++
+            ["{-# LINE " ++ show (startLine $ ann rhs) ++ " " ++ show (fileName $ ann rhs) ++ " #-}\n" ++
+             prettyPrint (PatBind an (toNamed $ "test" ++ show i) Nothing bod Nothing)
+            | (i, HintRule _ _ _ lhs rhs side _) <- zip [1..] matches, "notTypeSafe" `notElem` vars (maybeToList side)
+            , let vs = map toNamed $ nub $ filter isUnifyVar $ vars lhs ++ vars rhs
+            , let inner = InfixApp an (Paren an lhs) (toNamed "==>") (Paren an rhs)
+            , let bod = UnGuardedRhs an $ if null vs then inner else Lambda an vs inner]
