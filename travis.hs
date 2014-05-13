6ae264da6532d6d85af4c43757b05f57d648543a
Neil Mitchell <ndmitchell@gmail.com>
2014-05-13 20:26:08 +0100

    Make the full test run --quickcheck as standard

--- travis.hs
+++ travis.hs
@@ -3,7 +3,7 @@ import Neil
 
 main :: IO ()
 main = do
-    cmd "hlint test --typecheck"
+    cmd "hlint test --typecheck --quickcheck"
     (time,_) <- duration $ cmdCode "hlint src"
     putStrLn $ "Running HLint on self took " ++ show time ++ "s"
     cmd "ghc -threaded -rtsopts -isrc -i. src/Paths.hs src/Main.hs --make -O -prof -auto-all -caf-all"
