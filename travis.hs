4512a7c639ff8f297db8a9c499e6fd8e25946bb3
Neil Mitchell <ndmitchell@gmail.com>
2014-10-18 15:14:52 +0100

    Avoid using the neil library

--- travis.hs
+++ travis.hs
@@ -1,12 +1,15 @@
 
-import Neil
+import Control.Exception.Extra
+import System.Time.Extra
+import System.Process.Extra
+
 
 main :: IO ()
-main = neil $ do
-    retry 3 $ cmd "cabal install QuickCheck"
-    cmd "hlint test --typecheck --quickcheck"
-    (time,_) <- duration $ cmdCode "hlint src"
-    putStrLn $ "Running HLint on self took " ++ show time ++ "s"
-    cmd "ghc -threaded -rtsopts -isrc -i. src/Paths.hs src/Main.hs --make -O -prof -auto-all -caf-all"
-    cmdCode "src/Main src +RTS -p"
-    cmd "head -n32 Main.prof"
+main = do
+    retry 3 $ system_ "cabal install QuickCheck"
+    system_ "hlint test --typecheck --quickcheck"
+    (time,_) <- duration $ system "hlint src"
+    putStrLn $ "Running HLint on self took " ++ showDuration time
+    system_ "ghc -threaded -rtsopts -isrc -i. src/Paths.hs src/Main.hs --make -O -prof -auto-all -caf-all"
+    system  "src/Main src +RTS -p"
+    system_ "head -n32 Main.prof"
