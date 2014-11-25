96c93dca343209111522660b317480c70c5101a7
Neil Mitchell <ndmitchell@gmail.com>
2014-11-25 09:10:29 +0000

    #98, lots of work improving the HLint3 API

--- src/Language/Haskell/HLint3.hs
+++ src/Language/Haskell/HLint3.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections, PatternGuards #-}
 
 -- | /WARNING: This module represents the evolving second version of the HLint API./
 --   /It will be renamed to drop the "3" in the next major version./
@@ -15,18 +16,18 @@ module Language.Haskell.HLint3(
     Idea(..), Severity(..), Note(..),
     -- * Settings
     Classify(..),
-    getHLintDataDir,
-    autoSettings, findSettings, readSettings,
+    getHLintDataDir, autoSettings,
+    findSettings, readSettingsFile,
     -- * Hints
-    Hint(..), builtinHints,
-    HintRule(..), hintRules,
+    HintBuiltin(..), HintRule(..),
+    Hint(..), resolveHints,
     -- * Scopes
     Scope, scopeCreate, scopeMatch, scopeMove,
     -- * Haskell-src-exts
-    parseModuleEx, defaultParseFlags, ParseError(..), ParseFlags(..), CppFlags(..)
+    parseModuleEx, defaultParseFlags, parseFlagsAddFixities, ParseError(..), ParseFlags(..), CppFlags(..)
     ) where
 
-import Settings
+import Settings hiding (findSettings)
 import Idea
 import Apply
 import Hint.Type
@@ -34,40 +35,70 @@ import Hint.All
 import CmdLine
 import Paths_hlint
 
-import Control.Applicative
-import Data.Tuple.Extra
 import Data.List.Extra
 import Data.Maybe
-import Data.Monoid
 import System.FilePath
 
 
--- | Get the Cabal configured data directory of HLint
+-- | Get the Cabal configured data directory of HLint.
 getHLintDataDir :: IO FilePath
 getHLintDataDir = getDataDir
 
--- | The function produces a tuple containg 'ParseFlags' (for 'parseModuleEx'), and 'Classify' and 'Hint' for 'applyHints'.
+
+-- | The function produces a tuple containg 'ParseFlags' (for 'parseModuleEx'),
+--   and 'Classify' and 'Hint' for 'applyHints'.
 --   It approximates the normal HLint configuration steps, roughly:
 --
--- 1. Use 'findSettings' to find and load the HLint settings files.
+-- 1. Use 'findSettings' with 'readSettingsFile' to find and load the HLint settings files.
 --
--- 1. Use 'readSettings' to interpret the settings files, producing 'HintRule' values (@LHS ==> RHS@ replacements)
---   and 'Classify' values to assign 'Severity' ratings to hints.
+-- 1. Use 'parseFlagsAddFixities' and 'resolveHints' to transform the outputs of 'findSettings'.
 --
--- 1. Use 'builtinHints' and 'hintRules' to generate a 'Hint' value.
---
--- 1. Take all fixities from the 'findSettings' modules and put them in the 'ParseFlags'.
+--   If you want to do anything custom (e.g. using a different data directory, storing intermediate outputs,
+--   loading hints from a database) you are expected to copy and paste this function, then change it to your needs.
 autoSettings :: IO (ParseFlags, [Classify], Hint)
 autoSettings = do
-    dataDir <- getHLintDataDir
-    (builtin, matches) <- first resolveBuiltin <$> findSettings dataDir (dataDir </> "HLint.hs") Nothing
-    let (classify, rules) = second hintRules $ concatUnzip $ map readSettings matches
-    let fixities = getFixity =<< moduleDecls =<< matches
-    return (parseFlagsAddFixities fixities defaultParseFlags, classify, mconcat $ rules : builtin)
+    (fixities, classify, hints) <- findSettings (readSettingsFile Nothing) Nothing
+    return (parseFlagsAddFixities fixities defaultParseFlags, classify, resolveHints hints)
 
-resolveBuiltin :: [String] -> [Hint]
-resolveBuiltin builtin = map f $ nub $ concat [if x == "All" then map fst builtinHints else [x] | x <- builtin]
-    where f x = fromMaybe (error $ "Unknown builtin hints: HLint.Builtin." ++ x) $ lookup x builtinHints
+
+-- | Given a directory (or 'Nothing' to imply 'getHLintDataDir'), and a mdoule name
+--   (e.g. @HLint.Default@), find the settings file associated with it, returning the
+--   name of the file, and (optionally) the contents.
+--
+--   This function looks for all settings files starting with @HLint.@ in the directory
+--   argument, and all other files relative to the current directory.
+readSettingsFile :: Maybe FilePath -> String -> IO (FilePath, Maybe String)
+readSettingsFile dir x
+    | Just x <- "HLint." `stripPrefix` x = do
+        dir <- maybe getHLintDataDir return dir
+        return (dir </> x <.> "hs", Nothing)
+    | otherwise = return (x <.> "hs", Nothing)
+
+
+-- | Given a function to load a module (typically 'readSettingsFile'), and a module to start from
+--   (defaults to @HLint.HLint@) find the information from all settings files.
+findSettings :: (String -> IO (FilePath, Maybe String)) -> Maybe String -> IO ([Fixity], [Classify], [Either HintBuiltin HintRule])
+findSettings load start = do
+    (file,contents) <- load $ fromMaybe "HLint.HLint" start
+    let flags = addInfix defaultParseFlags
+    res <- parseModuleEx flags file contents
+    case res of
+        Left (ParseError sl msg err) ->
+            error $ "Settings parse failure at " ++ showSrcLoc sl ++ ": " ++ msg ++ "\n" ++ err
+        Right (m, _) -> do
+            imported <- sequence [f $ fromNamed $ importModule i | i <- moduleImports m, importPkg i `elem` [Just "hint", Just "hlint"]]
+            let (classify, rules) = Settings.readSettings m
+            let fixities = getFixity =<< moduleDecls m
+            return $ concatUnzip3 $ (fixities,classify,map Right rules) : imported
+    where
+        builtins =  [(drop 4 $ show h, h :: HintBuiltin) | h <- [minBound .. maxBound]]
+
+        f x | x == "HLint.Builtin.All" = return ([], [], map Left [minBound..maxBound])
+        f x | Just x <- "HLint.Builtin." `stripPrefix` x = case lookup x builtins of
+                Just x -> return ([], [], [Left x])
+                Nothing -> error $ "Unknown builtin hints: HLint.Builtin." ++ x
+            | otherwise = findSettings load (Just x)
+
 
 -- | Snippet from the documentation, if this changes, update the documentation
 _docs :: IO ()
