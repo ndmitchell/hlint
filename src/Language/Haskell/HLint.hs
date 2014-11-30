a9f93d057fb3f234f889749f2527877d628d5b27
Neil Mitchell <ndmitchell@gmail.com>
2014-11-30 20:07:29 +0000

    Add notes to HLint API that it is going away eventually

--- src/Language/Haskell/HLint.hs
+++ src/Language/Haskell/HLint.hs
@@ -1,7 +1,8 @@
 {-|
-This module provides a library interface to HLint. The current
-interface is strongly modelled on the command line interface,
-and is expected to evolve.
+/WARNING: This module represents the old version of the HLint API./
+/It will be deleted in favour of "Language.Haskell.HLint3" in the next major version./
+
+This module provides a library interface to HLint, strongly modelled on the command line interface.
 -}
 
 module Language.Haskell.HLint(module HLint) where
