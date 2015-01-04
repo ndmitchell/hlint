964e30de65efa2fa9bfaf64c9f861010b170e7ad
Neil Mitchell <ndmitchell@gmail.com>
2015-01-04 16:33:34 +0000

    #107, temporarily disable --quickcheck --typecheck tests

--- travis.hs
+++ travis.hs
@@ -7,6 +7,7 @@ import System.Process.Extra
 main :: IO ()
 main = do
     retry 3 $ system_ "cabal install QuickCheck"
-    system_ "hlint test --typecheck --quickcheck"
+    -- FIXME: Temporarily disabled due to GHC 7.10 issues
+    -- system_ "hlint test --typecheck --quickcheck"
     (time,_) <- duration $ system "hlint src"
     putStrLn $ "Running HLint on self took " ++ showDuration time
