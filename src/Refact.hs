68712902fa246ebba58abd477d16f0520442e0be
Matthew Pickering <matthewtpickering@gmail.com>
2015-07-21 11:53:27 +0200

    Updates for refact 0.2

--- src/Refact.hs
+++ src/Refact.hs
@@ -4,7 +4,10 @@ import qualified Refact.Types as R
 import HSE.All
 
 toRefactSrcSpan :: SrcSpan -> R.SrcSpan
-toRefactSrcSpan ss = R.SrcSpan (srcSpanStart ss) (srcSpanEnd ss)
+toRefactSrcSpan ss = R.SrcSpan (srcSpanStartLine ss)
+                               (srcSpanStartColumn ss)
+                               (srcSpanEndLine ss)
+                               (srcSpanEndColumn ss)
 
 toSS :: Annotated a => a S -> R.SrcSpan
 toSS = toRefactSrcSpan . toSrcSpan . ann
