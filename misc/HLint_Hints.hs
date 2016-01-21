3cab6b88649ca082dc3cd32096476845fe50c7af
Neil Mitchell <ndmitchell@gmail.com>
2016-01-21 18:27:18 +0000

    Drop the ignore warnings, no longer necessary

--- misc/HLint_Hints.hs
+++ misc/HLint_Hints.hs
@@ -17,9 +17,3 @@ error = ideaN Suggestion ==> suggestN
 ignore "Use toNamed" = Hint.Naming.shorten
 -- would require module cycles
 ignore "Use toNamed" = HSE.Util
-
--- good for beginners, but I sometimes use it to increase symmetry
-ignore "Use let"
-
--- I do this anyway where it makes sense
-ignore "Eta reduce"
