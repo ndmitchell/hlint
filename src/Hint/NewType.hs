a74616c0918b75212089320f2d598c95924d9e68
Neil Mitchell <ndmitchell@gmail.com>
2016-12-04 21:12:38 +0000

    #287, fix an incorrect newtype suggestion

--- src/Hint/NewType.hs
+++ src/Hint/NewType.hs
@@ -8,6 +8,7 @@ data Foo = Foo Int -- newtype Foo = Foo Int
 data Foo = Foo Int deriving (Show, Eq) -- newtype Foo = Foo Int deriving (Show, Eq)
 data Foo = Foo { field :: Int } deriving Show -- newtype Foo = Foo { field :: Int } deriving Show
 data Foo a b = Foo a -- newtype Foo a b = Foo a
+data Foo = Foo { field1, field2 :: Int}
 data S a = forall b . Show b => S b
 data Color a = Red a | Green a | Blue a
 data Pair a b = Pair a b
@@ -27,8 +28,8 @@ newtypeHintDecl d@(DataDecl sp (DataType dtA) ctx dclH
   case tvb of
     Just _  -> []
     Nothing -> case cd of
-                 (ConDecl _ _ [_]) -> wrn
-                 (RecDecl _ _ [_]) -> wrn
+                 ConDecl _ _ [_] -> wrn
+                 RecDecl _ _ [FieldDecl _ [_] _] -> wrn
                  _ -> []
   where suggestion = DataDecl sp (NewType dtA) ctx dclH [qcD] der
         wrn = [(suggestN "Use newtype instead of data" d suggestion){ideaNote = [DecreasesLaziness]}]
