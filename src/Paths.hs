93b1b9ae86ec08f24aaedac0f6f48f1449e554f9
Neil Mitchell <ndmitchell@gmail.com>
2017-04-27 17:59:23 +0100

    Clean up whitespace and comments in Paths

--- src/Paths.hs
+++ src/Paths.hs
@@ -1,15 +1,10 @@
-{-|
-    I would much rather put this module hidden away in src
-    but then Cabal will always use it in preference to the
-    auto-generated one.
--}
 
 module Paths_hlint(version, getDataDir) where
 
 import Data.Version.Extra
 
 version :: Version
-version =  makeVersion [0,0]
+version = makeVersion [0,0]
 
 getDataDir :: IO FilePath
 getDataDir = return "data"
