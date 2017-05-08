ccb2fabf289028723010e8dfef57af8d011a546c
Neil Mitchell <ndmitchell@gmail.com>
2017-05-08 23:31:28 +0100

    Update the CI to 2.0.6

--- misc/appveyor.bat
+++ misc/appveyor.bat
@@ -1,7 +1,7 @@
 setlocal
 rem Put curl on the PATH
 set PATH=C:\Program Files\Git\mingw64\bin;%PATH%
-set VERSION=2.0.5
+set VERSION=2.0.6
 mkdir %TEMP%\hlint
 curl -o%TEMP%\hlint\hlint.zip -L --insecure https://github.com/ndmitchell/hlint/releases/download/v%VERSION%/hlint-%VERSION%-x86_64-windows.zip
 7z x %TEMP%\hlint\hlint.zip -o%TEMP%\hlint -y
--- misc/travis.sh
+++ misc/travis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION=2.0.5
+VERSION=2.0.6
 TEMP=$(mktemp --directory .hlint-XXXXX)
 mkdir $TEMP
 curl -o$TEMP/hlint.tar.gz -L --insecure https://github.com/ndmitchell/hlint/releases/download/v$VERSION/hlint-$VERSION-x86_64-linux.tar.gz
