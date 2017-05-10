251d40fcce8144698be1d3050b0e4df317075144
Neil Mitchell <ndmitchell@gmail.com>
2017-05-10 20:47:56 +0100

    Rewrite the Travis spawning script to use the one in 'neil'

--- misc/travis.sh
+++ misc/travis.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-VERSION=2.0.6
-TEMP=$(mktemp --directory .hlint-XXXXX)
-mkdir $TEMP
-curl -o$TEMP/hlint.tar.gz -L --insecure https://github.com/ndmitchell/hlint/releases/download/v$VERSION/hlint-$VERSION-x86_64-linux.tar.gz
-tar -xzf $TEMP/hlint.tar.gz -C$TEMP
-$TEMP/hlint-$VERSION/hlint $*
+curl --location --silent https://raw.github.com/ndmitchell/neil/master/misc/travis.sh | sh -s -- hlint $*
