9c68393a0d888337dd1a36babd2f1fd365cd46b8
Neil Mitchell <ndmitchell@gmail.com>
2017-11-03 08:49:48 +0000

    Refactor unifyExp

--- src/HSE/Unify.hs
+++ src/HSE/Unify.hs
@@ -77,36 +77,41 @@ unifyDef nm x y = fmap mconcat . sequence =<< gzip (unify nm False) x y
 -- do not expand out a dot at the root, since otherwise you get two matches because of readRule (Bug #570)
 unifyExp :: NameMatch -> Bool -> Exp_ -> Exp_ -> Maybe (Subst Exp_)
 unifyExp nm root x y | isParen x || isParen y =
-  Subst . map (rebracket y) . fromSubst <$> unifyExp nm root (fromParen x) (fromParen y)
+    fmap (rebracket y) <$> unifyExp nm root (fromParen x) (fromParen y)
+    where
+        rebracket (Paren l e') e | e' == e = Paren l e
+        rebracket e e' = e'
+
 unifyExp nm root (Var _ (fromNamed -> v)) y | isUnifyVar v = Just $ Subst [(v,y)]
 unifyExp nm root (Var _ x) (Var _ y) | nm x y = Just mempty
+
+-- Options: match directly, and expand through .
 unifyExp nm root x@(App _ x1 x2) (App _ y1 y2) =
     liftM2 (<>) (unifyExp nm False x1 y1) (unifyExp nm False x2 y2) `mplus`
     (do guard $ not root; InfixApp _ y11 dot y12 <- return $ fromParen y1; guard $ isDot dot; unifyExp nm root x (App an y11 (App an y12 y2)))
+
+-- Options: match directly, then expand through $, then desugar infix
 unifyExp nm root x (InfixApp _ lhs2 op2 rhs2)
     | InfixApp _ lhs1 op1 rhs1 <- x = guard (nmOp nm op1 op2) >> liftM2 (<>) (unifyExp nm False lhs1 lhs2) (unifyExp nm False rhs1 rhs2)
     | isDol op2 = unifyExp nm root x $ App an lhs2 rhs2
     | otherwise = unifyExp nm root x $ App an (App an (opExp op2) lhs2) rhs2
-unifyExp nm root x y | isOther x, isOther y = unifyDef nm x y
-unifyExp nm root _ _ = Nothing
 
-rebracket (Paren l e') (v, e)
-  | e' == e = (v, Paren l e)
-rebracket e (v, e') = (v, e')
+unifyExp nm root x y | isOther x, isOther y = unifyDef nm x y
+    where
+        -- types that are not already handled in unify
+        {-# INLINE isOther #-}
+        isOther Var{} = False
+        isOther App{} = False
+        isOther InfixApp{} = False
+        isOther _ = True
+
+unifyExp nm root _ _ = Nothing
 
 
 unifyPat :: NameMatch -> Pat_ -> Pat_ -> Maybe (Subst Exp_)
 unifyPat nm (PVar _ x) (PVar _ y) = Just $ Subst [(fromNamed x, toNamed $ fromNamed y)]
 unifyPat nm (PVar _ x) PWildCard{} = Just $ Subst [(fromNamed x, toNamed $ "_" ++ fromNamed x)]
 unifyPat nm x y = unifyDef nm x y
-
-
--- types that are not already handled in unify
-{-# INLINE isOther #-}
-isOther Var{} = False
-isOther App{} = False
-isOther InfixApp{} = False
-isOther _ = True
 
 
 ---------------------------------------------------------------------
