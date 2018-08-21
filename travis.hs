f2fdaa4b1017bbda4faaba4e09236cbfea02d58d
Neil Mitchell <ndmitchell@gmail.com>
2018-08-21 17:02:33 +0100

    #514, disable stack test

--- travis.hs
+++ travis.hs
@@ -7,7 +7,9 @@ import System.Process.Extra
 main :: IO ()
 main = do
     -- retry 3 $ system_ "cabal install QuickCheck"
-    -- FIXME: Temporarily disabled due to GHC 7.10 issues
-    system_ "hlint test +RTS -K1K" --typecheck --quickcheck
+    -- FIXME: Temporarily disabled --typecheck --quickcheck due to GHC 7.10 issues
+    -- FIXME: reads 1M rather than 1K bceause of space leak in yaml-0.10.0
+    --        https://github.com/ndmitchell/hlint/issues/519
+    system_ "hlint test +RTS -K1M" --typecheck --quickcheck
     (time,_) <- duration $ system_ "UNIPLATE_VERBOSE=-1 hlint src +RTS -K1K"
     putStrLn $ "Running HLint on self took " ++ showDuration time
