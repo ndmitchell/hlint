ab1c9fa5f0ca38786ff525cf37c3b50723657af1
Neil Mitchell <ndmitchell@gmail.com>
2018-08-21 19:03:09 +0100

    Update travis.hs

--- travis.hs
+++ travis.hs
@@ -8,8 +8,8 @@ main :: IO ()
 main = do
     -- retry 3 $ system_ "cabal install QuickCheck"
     -- FIXME: Temporarily disabled --typecheck --quickcheck due to GHC 7.10 issues
-    -- FIXME: reads 1M rather than 1K bceause of space leak in yaml-0.10.0
+    -- FIXME: reads 1M in two places rather than 1K bceause of space leak in yaml-0.10.0
     --        https://github.com/ndmitchell/hlint/issues/519
     system_ "hlint test +RTS -K1M" --typecheck --quickcheck
-    (time,_) <- duration $ system_ "UNIPLATE_VERBOSE=-1 hlint src +RTS -K1K"
+    (time,_) <- duration $ system_ "UNIPLATE_VERBOSE=-1 hlint src +RTS -K1M"
     putStrLn $ "Running HLint on self took " ++ showDuration time
