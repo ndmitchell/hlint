33097d4b9cae3eb5e9a4a58b57c89c28c7097ea7
Neil Mitchell <ndmitchell@gmail.com>
2018-09-05 11:48:01 +0100

    #524, add a test

--- src/Hint/NewType.hs
+++ src/Hint/NewType.hs
@@ -13,6 +13,7 @@ data S a = forall b . Show b => S b
 data Color a = Red a | Green a | Blue a
 data Pair a b = Pair a b
 data Foo = Bar
+data Foo a = Eq a => MkFoo a
 data X = Y {-# UNPACK #-} !Int -- newtype X = Y Int
 data A = A {b :: !C} -- newtype A = A {b :: C}
 data A = A Int#
