ee8d4bad71ac66f5bfd075f17a9d8d7c2837dacc
Neil Mitchell <ndmitchell@gmail.com>
2019-04-16 16:21:53 +0100

    Shake all the space leaks out the Util by using a ReaderT IO

--- src/Test/Util.hs
+++ src/Test/Util.hs
@@ -8,23 +8,26 @@ module Test.Util(
 
 import Idea
 import Control.Monad
-import Control.Monad.Trans.State.Strict
+import Control.Monad.Trans.Reader
 import Control.Monad.IO.Class
+import Data.IORef
 
 
 data S = S
     {failures :: !Int
     ,total :: !Int
-    ,ideas :: [Idea]
+    ,ideas :: [[Idea]]
     }
 
-newtype Test a = Test (StateT S IO a)
+newtype Test a = Test (ReaderT (IORef S) IO a)
     deriving (Functor, Applicative, Monad, MonadIO)
 
 -- | Returns the number of failing tests.
 withTests :: Test a -> IO (Int, a)
 withTests (Test act) = do
-    (res, S{..}) <- runStateT act $ S 0 0 []
+    ref <- newIORef $ S 0 0 []
+    res <- runReaderT act ref
+    S{..} <- readIORef ref
     putStrLn ""
     putStrLn $ if failures == 0
         then "Tests passed (" ++ show total ++ ")"
@@ -32,21 +35,28 @@ withTests (Test act) = do
     return (failures, res)
 
 addIdeas :: [Idea] -> Test ()
-addIdeas xs = Test $ modify $ \s -> s{ideas = xs ++ ideas s}
+addIdeas xs = do
+    ref <- Test ask
+    liftIO $ modifyIORef' ref $ \s -> s{ideas = xs : ideas s}
 
 getIdeas :: Test [Idea]
-getIdeas = Test $ gets ideas
+getIdeas = do
+    ref <- Test ask
+    liftIO $ concat . reverse . ideas <$> readIORef ref
 
 progress :: Test ()
 progress = liftIO $ putChar '.'
 
 passed :: Test ()
-passed = Test $ modify $ \s -> s{total=total s+1}
+passed = do
+    ref <- Test ask
+    liftIO $ modifyIORef' ref $ \s -> s{total=total s+1}
 
 failed :: [String] -> Test ()
 failed xs = do
     unless (null xs) $ liftIO $ putStrLn $ unlines $ "" : xs
-    Test $ modify $ \s -> s{total=total s+1, failures=failures s+1}
+    ref <- Test ask
+    liftIO $ modifyIORef' ref $ \s -> s{total=total s+1, failures=failures s+1}
 
 tested :: Bool -> Test ()
 tested b = if b then passed else failed []
