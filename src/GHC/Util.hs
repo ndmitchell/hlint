b517f04039017fce89091666e0001c482962c313
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-05-29 13:25:38 -0400

    Implementation of 'isAtom' for GHC.

--- src/GHC/Util.hs
+++ src/GHC/Util.hs
@@ -9,9 +9,11 @@ module GHC.Util (
   , getMessages
   , SDoc
   , Located
+  , isAtom, paren
   ) where
 
 import "ghc-lib-parser" HsSyn
+import "ghc-lib-parser" BasicTypes
 import "ghc-lib-parser" DynFlags
 import "ghc-lib-parser" Platform
 import "ghc-lib-parser" Fingerprint
@@ -28,7 +30,6 @@ import "ghc-lib-parser" GHC.LanguageExtensions.Type
 import Data.List
 import System.FilePath
 import Language.Preprocessor.Unlit
-
 
 fakeSettings :: Settings
 fakeSettings = Settings
@@ -64,3 +65,49 @@ parseFileGhcLib filename str =
     buffer = stringToStringBuffer $
               if takeExtension filename /= ".lhs" then str else unlit filename str
     parseState = mkPState dynFlags buffer location
+
+-- | 'isAtom e' if 'e' requires no bracketing ever.
+isAtom :: HsExpr p -> Bool
+isAtom x = case x of
+  HsVar {} -> True
+  HsUnboundVar {}   -> True
+  HsRecFld {}       -> True
+  HsOverLabel {}    -> True
+  HsIPVar {}        -> True
+  HsPar {}          -> True
+  SectionL {}       -> True
+  SectionR {}       -> True
+  ExplicitTuple {}  -> True
+  ExplicitSum {}    -> True
+  ExplicitList {}   -> True
+  RecordCon {}      -> True
+  RecordUpd {}      -> True
+  ArithSeq {}       -> True
+  HsBracket {}      -> True
+  HsRnBracketOut {} -> True
+  HsTcBracketOut {} -> True
+  HsSpliceE {}      -> True
+  HsLit _ x     | not $ isNegativeLit x     -> True
+  HsOverLit _ x | not $ isNegativeOverLit x -> True
+  _                 -> False
+  where
+    isNegativeLit (HsInt _ i) = il_neg i
+    isNegativeLit (HsRat _ f _) = fl_neg f
+    isNegativeLit (HsFloatPrim _ f) = fl_neg f
+    isNegativeLit (HsDoublePrim _ f) = fl_neg f
+    isNegativeLit (HsIntPrim _ x) = x < 0
+    isNegativeLit (HsInt64Prim _ x) = x < 0
+    isNegativeLit (HsInteger _ x _) = x < 0
+    isNegativeLit _ = False
+
+    isNegativeOverLit (OverLit {ol_val=HsIntegral i}) = il_neg i
+    isNegativeOverLit (OverLit {ol_val=HsFractional f}) = fl_neg f
+    isNegativeOverLit _ = False
+
+-- | 'addParen e' wraps 'e' in parens.
+addParen :: HsExpr GhcPs -> HsExpr GhcPs
+addParen e = HsPar noExt (noLoc e)
+
+-- | 'paren e' wraps 'e' in parens if 'e' is non-atomic.
+paren :: HsExpr GhcPs -> HsExpr GhcPs
+paren x = if isAtom x then x else addParen x
