79348773870c143cc992a92e8e30ebca97afd153
Georgi Lyubenov <godzbanebane@gmail.com>
2019-07-03 22:14:13 +0300

    Remove qualified imports

--- src/Hint/NewType.hs
+++ src/Hint/NewType.hs
@@ -31,47 +31,47 @@ newtype Foo = Foo Int deriving stock Show
 -}
 module Hint.NewType (newtypeHint) where
 
-import Hint.Type
+import Hint.Type (Idea, DeclHint', Note(DecreasesLaziness), ideaNote, ignoreNoSuggestion', suggestN')
 
 import Data.List (isSuffixOf)
 -- TODO: remove these qualifieds
-import qualified "ghc-lib-parser" HsDecls as Hs
-import qualified "ghc-lib-parser" HsSyn   as Hs
-import qualified "ghc-lib-parser" Outputable as Hs
-import qualified "ghc-lib-parser" SrcLoc  as Hs
+import "ghc-lib-parser" HsDecls
+import "ghc-lib-parser" HsSyn
+import "ghc-lib-parser" Outputable
+import "ghc-lib-parser" SrcLoc
 
 -- TODO: get deriving strategies to work
 newtypeHint :: DeclHint'
 newtypeHint _ _ x = newtypeHintDecl x ++ newTypeDerivingStrategiesHintDecl x
 
-newtypeHintDecl :: Hs.LHsDecl Hs.GhcPs -> [Idea]
+newtypeHintDecl :: LHsDecl GhcPs -> [Idea]
 newtypeHintDecl old
     | Just WarnNewtype{newDecl, insideType} <- singleSimpleFieldNew old
     = [(suggestN' "Use newtype instead of data" old newDecl)
             {ideaNote = [DecreasesLaziness | warnBang insideType]}]
 newtypeHintDecl _ = []
 
-newTypeDerivingStrategiesHintDecl :: Hs.LHsDecl Hs.GhcPs -> [Idea]
-newTypeDerivingStrategiesHintDecl decl@(Hs.L _ (Hs.TyClD _ (Hs.DataDecl _ _ _ _ dataDef))) =
+newTypeDerivingStrategiesHintDecl :: LHsDecl GhcPs -> [Idea]
+newTypeDerivingStrategiesHintDecl decl@(L _ (TyClD _ (DataDecl _ _ _ _ dataDef))) =
     [ignoreNoSuggestion' "Use DerivingStrategies" decl | not $ isData dataDef, not $ hasAllStrategies dataDef]
 newTypeDerivingStrategiesHintDecl _ = []
 
-hasAllStrategies :: Hs.HsDataDefn Hs.GhcPs -> Bool
-hasAllStrategies (Hs.HsDataDefn _ Hs.NewType _ _ _ _ (Hs.L _ xs)) = all hasStrategyClause xs
+hasAllStrategies :: HsDataDefn GhcPs -> Bool
+hasAllStrategies (HsDataDefn _ NewType _ _ _ _ (L _ xs)) = all hasStrategyClause xs
 hasAllStrategies _ = False
 
-isData :: Hs.HsDataDefn Hs.GhcPs -> Bool
-isData (Hs.HsDataDefn _ Hs.NewType _ _ _ _ _) = False
-isData (Hs.HsDataDefn _ Hs.DataType _ _ _ _ _) = True
+isData :: HsDataDefn GhcPs -> Bool
+isData (HsDataDefn _ NewType _ _ _ _ _) = False
+isData (HsDataDefn _ DataType _ _ _ _ _) = True
 isData _ = False
 
-hasStrategyClause :: Hs.LHsDerivingClause Hs.GhcPs -> Bool
-hasStrategyClause (Hs.L _ (Hs.HsDerivingClause _ (Just _) _)) = True
+hasStrategyClause :: LHsDerivingClause GhcPs -> Bool
+hasStrategyClause (L _ (HsDerivingClause _ (Just _) _)) = True
 hasStrategyClause _ = False
 
 data WarnNewtype = WarnNewtype
-    { newDecl :: Hs.LHsDecl Hs.GhcPs
-    , insideType :: Hs.HsType Hs.GhcPs
+    { newDecl :: LHsDecl GhcPs
+    , insideType :: HsType GhcPs
     }
 
 -- | Given a declaration, returns the suggested \"newtype\"ized declaration following these guidelines:
@@ -81,13 +81,13 @@ data WarnNewtype = WarnNewtype
 -- * Single field constructors get newtyped - @data X = X Int@ -> @newtype X = X Int@
 -- * Single record field constructors get newtyped - @data X = X {getX :: Int}@ -> @newtype X = X {getX :: Int}@
 -- * All other declarations are ignored.
-singleSimpleFieldNew :: Hs.LHsDecl Hs.GhcPs -> Maybe WarnNewtype
-singleSimpleFieldNew (Hs.L loc (Hs.TyClD ext decl@(Hs.DataDecl _ _ _ _ dataDef@(Hs.HsDataDefn _ Hs.DataType _ _ _ [Hs.L _ constructor] _))))
+singleSimpleFieldNew :: LHsDecl GhcPs -> Maybe WarnNewtype
+singleSimpleFieldNew (L loc (TyClD ext decl@(DataDecl _ _ _ _ dataDef@(HsDataDefn _ DataType _ _ _ [L _ constructor] _))))
     | Just inType <- simpleCons constructor =
         Just WarnNewtype
-              { newDecl = Hs.L loc $ Hs.TyClD ext decl {Hs.tcdDataDefn = dataDef
-                  { Hs.dd_ND = Hs.NewType
-                  , Hs.dd_cons = map (\(Hs.L consloc x) -> Hs.L consloc $ dropConsBang x) $ Hs.dd_cons dataDef
+              { newDecl = L loc $ TyClD ext decl {tcdDataDefn = dataDef
+                  { dd_ND = NewType
+                  , dd_cons = map (\(L consloc x) -> L consloc $ dropConsBang x) $ dd_cons dataDef
                   }}
               , insideType = inType
               }
@@ -97,48 +97,48 @@ singleSimpleFieldNew _ = Nothing
 --
 -- | Checks whether its argument is a \"simple constructor\" (see criteria in 'singleSimpleFieldNew')
 -- returning the type inside the constructor if it is. This is needed for bang/MagicHash analysis.
-simpleCons :: Hs.ConDecl Hs.GhcPs -> Maybe (Hs.HsType Hs.GhcPs)
-simpleCons (Hs.ConDeclH98 _ _ _ [] context (Hs.PrefixCon [Hs.L _ inType]) _)
+simpleCons :: ConDecl GhcPs -> Maybe (HsType GhcPs)
+simpleCons (ConDeclH98 _ _ _ [] context (PrefixCon [L _ inType]) _)
     | emptyOrNoContext context
     , not $ isUnboxedTuple inType
     , not $ isHashy inType
     = Just inType
-simpleCons (Hs.ConDeclH98 _ _ _ [] context (Hs.RecCon (Hs.L _ [Hs.L _ (Hs.ConDeclField _ [_] (Hs.L _ inType) _)])) _)
+simpleCons (ConDeclH98 _ _ _ [] context (RecCon (L _ [L _ (ConDeclField _ [_] (L _ inType) _)])) _)
     | emptyOrNoContext context
     , not $ isUnboxedTuple inType
     , not $ isHashy inType
     = Just inType
 simpleCons _ = Nothing
 
-isHashy :: Hs.HsType Hs.GhcPs -> Bool
-isHashy (Hs.HsTyVar _ _ identifier) = "#" `isSuffixOf` Hs.showSDocUnsafe (Hs.ppr identifier)
+isHashy :: HsType GhcPs -> Bool
+isHashy (HsTyVar _ _ identifier) = "#" `isSuffixOf` showSDocUnsafe (ppr identifier)
 isHashy _ = False
 
-warnBang :: Hs.HsType Hs.GhcPs -> Bool
-warnBang (Hs.HsBangTy _ (Hs.HsSrcBang _ _ Hs.SrcStrict) _) = False
+warnBang :: HsType GhcPs -> Bool
+warnBang (HsBangTy _ (HsSrcBang _ _ SrcStrict) _) = False
 warnBang _ = True
 
-emptyOrNoContext :: Maybe (Hs.LHsContext Hs.GhcPs) -> Bool
+emptyOrNoContext :: Maybe (LHsContext GhcPs) -> Bool
 emptyOrNoContext Nothing = True
-emptyOrNoContext (Just (Hs.L _ [])) = True
+emptyOrNoContext (Just (L _ [])) = True
 emptyOrNoContext _ = False
 
 -- | The \"Bang\" here refers to 'HsSrcBang', which notably also include @UNPACK@ pragmas!
-dropConsBang :: Hs.ConDecl Hs.GhcPs -> Hs.ConDecl Hs.GhcPs
-dropConsBang decl@(Hs.ConDeclH98 _ _ _ _ _ (Hs.PrefixCon fields) _) =
-    decl {Hs.con_args = Hs.PrefixCon $ map Hs.getBangType fields}
-dropConsBang decl@(Hs.ConDeclH98 _ _ _ _ _ (Hs.RecCon (Hs.L recloc conDeclFields)) _) =
-    decl {Hs.con_args = Hs.RecCon $ Hs.L recloc $ removeUnpacksRecords conDeclFields}
+dropConsBang :: ConDecl GhcPs -> ConDecl GhcPs
+dropConsBang decl@(ConDeclH98 _ _ _ _ _ (PrefixCon fields) _) =
+    decl {con_args = PrefixCon $ map getBangType fields}
+dropConsBang decl@(ConDeclH98 _ _ _ _ _ (RecCon (L recloc conDeclFields)) _) =
+    decl {con_args = RecCon $ L recloc $ removeUnpacksRecords conDeclFields}
     where
-        removeUnpacksRecords :: [Hs.LConDeclField Hs.GhcPs] -> [Hs.LConDeclField Hs.GhcPs]
-        removeUnpacksRecords = map (\(Hs.L conDeclFieldLoc x) -> Hs.L conDeclFieldLoc $ removeConDeclFieldUnpacks x)
+        removeUnpacksRecords :: [LConDeclField GhcPs] -> [LConDeclField GhcPs]
+        removeUnpacksRecords = map (\(L conDeclFieldLoc x) -> L conDeclFieldLoc $ removeConDeclFieldUnpacks x)
 
-        removeConDeclFieldUnpacks :: Hs.ConDeclField Hs.GhcPs -> Hs.ConDeclField Hs.GhcPs
-        removeConDeclFieldUnpacks conDeclField@(Hs.ConDeclField _ _ fieldType _) =
-            conDeclField {Hs.cd_fld_type = Hs.getBangType fieldType}
+        removeConDeclFieldUnpacks :: ConDeclField GhcPs -> ConDeclField GhcPs
+        removeConDeclFieldUnpacks conDeclField@(ConDeclField _ _ fieldType _) =
+            conDeclField {cd_fld_type = getBangType fieldType}
         removeConDeclFieldUnpacks x = x
 dropConsBang x = x
 
-isUnboxedTuple :: Hs.HsType Hs.GhcPs -> Bool
-isUnboxedTuple (Hs.HsTupleTy _ Hs.HsUnboxedTuple _) = True
+isUnboxedTuple :: HsType GhcPs -> Bool
+isUnboxedTuple (HsTupleTy _ HsUnboxedTuple _) = True
 isUnboxedTuple _ = False
