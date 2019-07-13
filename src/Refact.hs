8df4ff7a8317219c7d0ec11ac2e3f03fec472c63
Georgi Lyubenov <godzbanebane@gmail.com>
2019-07-13 21:56:52 +0300

    Add a function for HasSrcSpan a -> Refact.SrcSpan

--- src/Refact.hs
+++ src/Refact.hs
@@ -1,7 +1,14 @@
-module Refact(toRefactSrcSpan, toSS) where
+{-# LANGUAGE PackageImports #-}
+module Refact
+    ( toRefactSrcSpan
+    , toSS
+    , toSrcSpan'
+    ) where
 
 import qualified Refact.Types as R
 import HSE.All
+
+import qualified "ghc-lib-parser" SrcLoc as Hs
 
 toRefactSrcSpan :: SrcSpan -> R.SrcSpan
 toRefactSrcSpan ss = R.SrcSpan (srcSpanStartLine ss)
@@ -11,3 +18,15 @@ toRefactSrcSpan ss = R.SrcSpan (srcSpanStartLine ss)
 
 toSS :: Annotated a => a S -> R.SrcSpan
 toSS = toRefactSrcSpan . srcInfoSpan . ann
+
+toSrcSpan' :: Hs.HasSrcSpan a => a -> R.SrcSpan
+toSrcSpan' x =
+    R.SrcSpan (Hs.srcSpanStartLine span)
+              (Hs.srcSpanStartCol span)
+              (Hs.srcSpanEndLine span)
+              (Hs.srcSpanEndCol span)
+    where
+        span :: Hs.RealSrcSpan
+        span = case Hs.getLoc x of
+                 Hs.RealSrcSpan s -> s
+                 _ -> error "Got a bad ghc SrcSpan! Report a bug to hlint please!"
