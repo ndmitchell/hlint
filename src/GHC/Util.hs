8ad867aae9f0b7380597a257be7b05b90188fee0
Georgi Lyubenov <godzbanebane@gmail.com>
2019-07-17 22:03:27 +0300

    Add a comment clarifying unsafePrettyPrint

--- src/GHC/Util.hs
+++ src/GHC/Util.hs
@@ -234,5 +234,9 @@ declName (ForD _ ForeignImport{fd_name}) = occNameString $ occName $ unLoc fd_na
 declName (ForD _ ForeignExport{fd_name}) = occNameString $ occName $ unLoc fd_name
 declName _ = ""
 
+-- \"Unsafely\" in this case means that it uses the following 'DynFlags' for printing -
+-- <http://hackage.haskell.org/package/ghc-lib-parser-8.8.0.20190424/docs/src/DynFlags.html#v_unsafeGlobalDynFlags unsafeGlobalDynFlags>
+-- This could lead to the issues documented there, but it also might not be a problem for our use case.
+-- TODO: Decide whether this really is unsafe, and if it is, what needs to be done to make it safe.
 unsafePrettyPrint :: (Outputable.Outputable a) => a -> String
 unsafePrettyPrint = Outputable.showSDocUnsafe . Outputable.ppr
