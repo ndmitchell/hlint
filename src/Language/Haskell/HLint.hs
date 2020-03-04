39740ed0eee1acd0b6f1d08286bd9a3155cea982
Neil Mitchell <ndmitchell@gmail.com>
2020-03-04 19:10:37 +0000

    Change the HLint API to use GHC types

--- src/Language/Haskell/HLint.hs
+++ src/Language/Haskell/HLint.hs
@@ -10,9 +10,7 @@ module Language.Haskell.HLint(hlint, Suggestion, suggestionLocation, suggestionS
 import qualified HLint
 import Config.Type
 import Idea
-import HSE.All
-
-import qualified SrcLoc as GHC
+import SrcLoc(SrcSpan)
 
 
 -- | This function takes a list of command line arguments, and returns the given suggestions.
@@ -36,8 +34,8 @@ instance Show Suggestion where
     show = show . fromSuggestion
 
 -- | From a suggestion, extract the file location it refers to.
-suggestionLocation :: Suggestion -> SrcLoc
-suggestionLocation = ghcSrcLocToHSE . GHC.srcSpanStart . ideaSpan . fromSuggestion
+suggestionLocation :: Suggestion -> SrcSpan
+suggestionLocation = ideaSpan . fromSuggestion
 
 
 -- | From a suggestion, determine how severe it is.
