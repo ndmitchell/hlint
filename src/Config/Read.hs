297ab84478d642aedd993b6c90130ddb8ba94962
Neil Mitchell <ndmitchell@gmail.com>
2020-03-08 17:45:10 +0000

    Make the failure to read Haskell config message clearer

--- src/Config/Read.hs
+++ src/Config/Read.hs
@@ -14,6 +14,7 @@ readFilesConfig files = do
     let (yaml, haskell) = partition (\(x,_) -> lower (takeExtension x) `elem` [".yml",".yaml"]) files
     unless (null haskell) $
         errorIO $ "HLint 2.3 and beyond cannot use Haskell configuration files.\n" ++
+                  "Tried to use: " ++ show haskell ++ "\n" ++
                   "Convert it to .yaml file format, following the example at\n" ++
                   "  <https://github.com/ndmitchell/hlint/blob/master/data/hlint.yaml>"
     yaml <- mapM (uncurry readFileConfigYaml) yaml
