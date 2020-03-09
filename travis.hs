f5a7f1d9bf5a436baaa146e0e17f71d14e55cce8
Neil Mitchell <ndmitchell@gmail.com>
2020-03-09 20:35:51 +0000

    Enable the extra/future groups of tests

--- travis.hs
+++ travis.hs
@@ -8,5 +8,5 @@ main = do
     -- retry 3 $ system_ "cabal install QuickCheck"
     -- FIXME: Temporarily disabled --typecheck --quickcheck due to GHC 7.10 issues
     system_ "hlint test +RTS -K512K" --typecheck --quickcheck
-    (time,_) <- duration $ system_ "hlint src" --  --with-group=extra" -- "UNIPLATE_VERBOSE=-1 hlint src +RTS -K1K"
+    (time,_) <- duration $ system_ "hlint src --with-group=extra --with-group=future" -- "UNIPLATE_VERBOSE=-1 hlint src +RTS -K1K"
     putStrLn $ "Running HLint on self took " ++ showDuration time
