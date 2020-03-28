bacbb123650ec4facd8c1ee5cad23fa9ea63b092
Neil Mitchell <ndmitchell@gmail.com>
2020-03-28 11:51:29 +0000

    Delete the old API's

--- src/Language/Haskell/HLint3.hs
+++
@@ -1,7 +0,0 @@
-
--- | A reexport of "Language.Haskell.HLint4" for compatibility purposes.
-module Language.Haskell.HLint3(
-    module Language.Haskell.HLint4
-    ) where
-
-import Language.Haskell.HLint4
