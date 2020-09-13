466ed41c491eff616281adc46b104b6045e6c8c8
Neil Mitchell <ndmitchell@gmail.com>
2020-09-13 11:46:28 +0100

    #1124, use --test in travis.hs

--- travis.hs
+++ travis.hs
@@ -5,8 +5,6 @@ import System.Process.Extra
 
 main :: IO ()
 main = do
-    -- retry 3 $ system_ "cabal install QuickCheck"
-    -- FIXME: Temporarily disabled --typecheck --quickcheck due to GHC 7.10 issues
-    system_ "hlint test +RTS -K512K" --typecheck --quickcheck
+    system_ "hlint --test +RTS -K512K"
     (time,_) <- duration $ system_ "hlint src --with-group=extra --with-group=future" -- "UNIPLATE_VERBOSE=-1 hlint src +RTS -K1K"
     putStrLn $ "Running HLint on self took " ++ showDuration time
