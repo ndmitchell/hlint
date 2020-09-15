b8cbda879a618ad48afec8dca2840262a2f1221f
Neil Mitchell <ndmitchell@gmail.com>
2020-09-15 22:26:43 +0100

    Make sure Travis runs the refactor tests

--- travis.hs
+++ travis.hs
@@ -5,6 +5,7 @@ import System.Process.Extra
 
 main :: IO ()
 main = do
+    system_ "cabal install apply-refact"
     system_ "hlint --test +RTS -K512K"
     (time,_) <- duration $ system_ "hlint src --with-group=extra --with-group=future" -- "UNIPLATE_VERBOSE=-1 hlint src +RTS -K1K"
     putStrLn $ "Running HLint on self took " ++ showDuration time
