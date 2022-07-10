8e8bb91b23753f721d8fa360fcb1a5752bbc2c03
Neil Mitchell <ndmitchell@gmail.com>
2022-07-10 19:29:26 +0100

    #1345, adjust the name of the flag, adapt help output to make it more readable

--- src/CmdLine.hs
+++ src/CmdLine.hs
@@ -169,7 +169,7 @@ mode = cmdArgsMode $ modes
         ,cmdIgnoreGlob = nam_ "ignore-glob" &= help "Ignore paths matching glob pattern (e.g. foo/bar/*.hs)"
         ,cmdGenerateMdSummary = nam_ "generate-summary" &= opt "hints.md" &= help "Generate a summary of available hints, in Mardown format"
         ,cmdGenerateJsonSummary = nam_ "generate-summary-json" &= opt "hints.json" &= help "Generate a summary of available hints, in JSON format"
-        ,cmdGenerateExhaustiveConf = nam_ "generate-exhaustive-config" &= opt Warning &= typ "ignore/suggest/warn/error" &= help "Generate a .hlint.yaml config file with all hints set to the specified severity level (default level: warning)"
+        ,cmdGenerateExhaustiveConf = nam_ "generate-config" &= opt Warning &= typ "LEVEL" &= help "Generate a .hlint.yaml config file with all hints set to the specified severity level (default level: warn, alternatives: ignore, suggest, error)"
         ,cmdTest = nam_ "test" &= help "Run the test suite"
         } &= auto &= explicit &= name "lint"
         &= details ["HLint gives hints on how to improve Haskell code."
