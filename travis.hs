3111617d64c20ce0701cd14f69d60c32f8a7326c
Neil Mitchell <ndmitchell@gmail.com>
2022-07-10 18:29:54 +0100

    Use the .hlint.yaml to enable additional hints, rather than travis.hs

--- travis.hs
+++ travis.hs
@@ -7,5 +7,5 @@ main :: IO ()
 main = do
     system_ "hlint --generate-summary"
     system_ "hlint --test +RTS -K512K"
-    (time,_) <- duration $ system_ "hlint src --with-group=extra --with-group=future" -- "UNIPLATE_VERBOSE=-1 hlint src +RTS -K1K"
+    (time,_) <- duration $ system_ "hlint src" -- "UNIPLATE_VERBOSE=-1 hlint src +RTS -K1K"
     putStrLn $ "Running HLint on self took " ++ showDuration time
